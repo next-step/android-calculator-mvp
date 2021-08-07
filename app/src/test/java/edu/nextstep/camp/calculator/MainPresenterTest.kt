5b482b60fe0760ab0c93756b2512cad2f294b55f
강현석 <kekemusa37@gmail.com>
2021-08-07 19:08:54 +0900

    :white_check_mark: test: presenter 변수 제거

--- app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
@@ -3,21 +3,16 @@ package edu.nextstep.camp.calculator
 import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class MainPresenterTest {
 
     private val expression: Expression = Expression.EMPTY
-    private lateinit var presenter: MainPresenter
-
-    @BeforeEach
-    fun setup() {
-        presenter = MainPresenter(expression = expression)
-    }
 
     @Test
     fun `숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
+        val presenter = MainPresenter(expression = expression)
+
         val actual = presenter
             .expression(number = 1)
             .toString()
