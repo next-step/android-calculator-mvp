f26518abb3a19d10a958a4dc12243660620a3e4e
강현석 <kekemusa37@gmail.com>
2021-08-07 18:09:58 +0900

    :sparkles: feat: [추가] 숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다 테스트 함수 및 expression 함수

--- app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
@@ -1,5 +1,13 @@
 package edu.nextstep.camp.calculator
 
-class MainPresenter {
+import edu.nextstep.camp.calculator.domain.Expression
 
+class MainPresenter(
+    private var expression: Expression = Expression.EMPTY
+) {
+
+    fun expression(number: Int): Expression {
+        expression += number
+        return expression
+    }
 }
