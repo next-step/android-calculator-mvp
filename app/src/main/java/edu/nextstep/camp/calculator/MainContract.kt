9f21b651510bb9ab8476d9d79d42719750235237
Joseph <wjsdudtkd95@gmail.com>
2021-08-19 20:04:59 +0900

    feat(step2): MainContract에 CalculateRecordRecyclerview를 위한 메서드 선언

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -1,5 +1,6 @@
 package edu.nextstep.camp.calculator
 
+import com.joseph.domain.CalculateRecord
 import com.joseph.domain.Expression
 import com.joseph.domain.Operator
 
@@ -7,6 +8,8 @@ interface MainContract {
     interface View {
         fun displayExpression(expression: Expression)
         fun showIncompleteExpressionToast()
+        fun showCalculateResults()
+        fun refreshRecyclerView(records: List<CalculateRecord>)
     }
 
     interface Presenter {
@@ -14,5 +17,7 @@ interface MainContract {
         fun addExpression(operator: Operator)
         fun removeAtLastExpression()
         fun calculate()
+        fun showCalculateResults()
+        fun addCalculateRecord(record: CalculateRecord)
     }
 }
