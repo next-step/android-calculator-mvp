a62825bef5ac7cd04b1f84371f6790c8492e3d51
ganghee <idiosauri@naver.com>
2022-01-29 17:33:28 +0900

    test: Presenter 에러처리에 대한 테스트 코드 작성

--- app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
@@ -1,5 +1,8 @@
 package edu.nextstep.camp.calculator
 
+import edu.nextstep.camp.calculator.domain.Calculator.Companion.IS_NOT_OPERATOR
+import edu.nextstep.camp.calculator.domain.Calculator.Companion.IS_NOT_OR_BLANK
+import edu.nextstep.camp.calculator.domain.Calculator.Companion.WRONG_INPUT
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
@@ -49,5 +52,32 @@ class MainPresenterTest {
 
         // THEN
         verify { view.showExpression("1 + 2 -") }
+    }
+
+    @Test
+    fun `빈 수식 입력시 예외처리를 해야한다`() {
+        // WHEN
+        presenter.calculate("")
+
+        // THEN
+        verify { view.showError(IS_NOT_OR_BLANK) }
+    }
+
+    @Test
+    fun `잘 못된 수식 입력시 예외처리를 해야한다`() {
+        // WHEN
+        presenter.calculate("1 +")
+
+        // THEN
+        verify { view.showError(WRONG_INPUT) }
+    }
+
+    @Test
+    fun `사칙 연산이 아닌 수식 입력시 예외처리를 해야한다`() {
+        // WHEN
+        presenter.calculate("1 $ 1")
+
+        // THEN
+        verify { view.showError(IS_NOT_OPERATOR) }
     }
 }
