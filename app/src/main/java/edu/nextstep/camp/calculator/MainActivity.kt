5d69bd874e507e415bda50d137dcfc3b0c828da2
Woogear Kwon <woogear.kwon@huray.net>
2022-01-29 18:32:26 +0900

    [Refactor] setOnClickListener 함수 로직 리팩토링

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,12 +1,14 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 import edu.nextstep.camp.calculator.domain.Calculator
 import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,74 +20,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener {
-            expression += 0
+        setViewsClickListener()
+    }
+
+    private fun setViewsClickListener() {
+        binding.run {
+            setNumberButtonsClickListener(
+                button0, button1, button2, button3, button4, button5, button6, button7, button8,
+                button9,
+            )
+            setOperatorButtonClickListener(buttonPlus, buttonMinus, buttonMultiply, buttonDivide)
+            buttonDelete.setOnClickListener { setDeleteButtonClickListener() }
+            buttonEquals.setOnClickListener { setEqualsButtonClickListener() }
+        }
+    }
+
+    private fun setNumberButtonsClickListener(vararg buttons: Button) {
+        buttons.forEach {
+            expression += it.text.toString().toInt()
             binding.textView.text = expression.toString()
         }
-        binding.button1.setOnClickListener {
-            expression += 1
-            binding.textView.text = expression.toString()
-        }
-        binding.button2.setOnClickListener {
-            expression += 2
-            binding.textView.text = expression.toString()
-        }
-        binding.button3.setOnClickListener {
-            expression += 3
-            binding.textView.text = expression.toString()
-        }
-        binding.button4.setOnClickListener {
-            expression += 4
-            binding.textView.text = expression.toString()
-        }
-        binding.button5.setOnClickListener {
-            expression += 5
-            binding.textView.text = expression.toString()
-        }
-        binding.button6.setOnClickListener {
-            expression += 6
-            binding.textView.text = expression.toString()
-        }
-        binding.button7.setOnClickListener {
-            expression += 7
-            binding.textView.text = expression.toString()
-        }
-        binding.button8.setOnClickListener {
-            expression += 8
-            binding.textView.text = expression.toString()
-        }
-        binding.button9.setOnClickListener {
-            expression += 9
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonPlus.setOnClickListener {
-            expression += Operator.Plus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMinus.setOnClickListener {
-            expression += Operator.Minus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMultiply.setOnClickListener {
-            expression += Operator.Multiply
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDivide.setOnClickListener {
-            expression += Operator.Divide
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDelete.setOnClickListener {
-            expression = expression.removeLast()
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonEquals.setOnClickListener {
-            val result = calculator.calculate(expression.toString())
-            if (result == null) {
-                Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
+    }
+
+    private fun setOperatorButtonClickListener(vararg buttons: Button) {
+        buttons.forEach {
+            expression += when {
+                (it.text.toString() == getString(R.string.calculator_plus)) -> Operator.Plus
+                (it.text.toString() == getString(R.string.calculator_minus)) -> Operator.Minus
+                (it.text.toString() == getString(R.string.calculator_multiply)) -> Operator.Multiply
+                (it.text.toString() == getString(R.string.calculator_divide)) -> Operator.Divide
+                else -> throw IllegalArgumentException("${it.text} is not an operator")
             }
-            expression = Expression.EMPTY + result
-            binding.textView.text = result.toString()
+
+            binding.textView.text = expression.toString()
         }
+    }
+
+    private fun setDeleteButtonClickListener() {
+        expression = expression.removeLast()
+        binding.textView.text = expression.toString()
+    }
+
+    private fun setEqualsButtonClickListener() {
+        val result = calculator.calculate(expression.toString())
+
+        if (result == null) {
+            Toast.makeText(
+                this@MainActivity,
+                R.string.incomplete_expression,
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        expression = Expression.EMPTY + result
+        binding.textView.text = result.toString()
     }
 }
