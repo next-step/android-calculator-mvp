22573ef14f83b2a2651eefa073ef59a84a13d283
Woogear Kwon <woogear.kwon@huray.net>
2022-01-30 10:05:05 +0900

    [Feat] MVP 패턴 적용 및 테스트 코드 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -8,18 +8,20 @@ import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 import edu.nextstep.camp.calculator.domain.Calculator
 import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
-import java.lang.IllegalArgumentException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var binding: ActivityMainBinding
     private val calculator = Calculator()
     private var expression = Expression.EMPTY
+
+    private lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        presenter = MainPresenter(this)
         setViewsClickListener()
     }
 
@@ -29,51 +31,45 @@ class MainActivity : AppCompatActivity() {
                 button0, button1, button2, button3, button4, button5, button6, button7, button8,
                 button9,
             )
-            setOperatorButtonClickListener(buttonPlus, buttonMinus, buttonMultiply, buttonDivide)
+            buttonPlus.setOnClickListener { setOperatorButtonClickListener(Operator.Plus) }
+            buttonMinus.setOnClickListener { setOperatorButtonClickListener(Operator.Minus) }
+            buttonMultiply.setOnClickListener { setOperatorButtonClickListener(Operator.Multiply) }
+            buttonDivide.setOnClickListener { setOperatorButtonClickListener(Operator.Divide) }
             buttonDelete.setOnClickListener { setDeleteButtonClickListener() }
             buttonEquals.setOnClickListener { setEqualsButtonClickListener() }
         }
     }
 
     private fun setNumberButtonsClickListener(vararg buttons: Button) {
-        buttons.forEach {
-            expression += it.text.toString().toInt()
-            binding.textView.text = expression.toString()
+        buttons.forEach { it ->
+            it.setOnClickListener {
+                presenter.addNumber(expression, (it as Button).text.toString())
+            }
         }
     }
 
-    private fun setOperatorButtonClickListener(vararg buttons: Button) {
-        buttons.forEach {
-            expression += when {
-                (it.text.toString() == getString(R.string.calculator_plus)) -> Operator.Plus
-                (it.text.toString() == getString(R.string.calculator_minus)) -> Operator.Minus
-                (it.text.toString() == getString(R.string.calculator_multiply)) -> Operator.Multiply
-                (it.text.toString() == getString(R.string.calculator_divide)) -> Operator.Divide
-                else -> throw IllegalArgumentException("${it.text} is not an operator")
-            }
-
-            binding.textView.text = expression.toString()
-        }
+    private fun setOperatorButtonClickListener(operator: Operator) {
+        presenter.addOperator(expression, operator)
     }
 
     private fun setDeleteButtonClickListener() {
-        expression = expression.removeLast()
-        binding.textView.text = expression.toString()
+        presenter.delete(expression)
     }
 
     private fun setEqualsButtonClickListener() {
-        val result = calculator.calculate(expression.toString())
+        presenter.calculate(expression)
+    }
 
-        if (result == null) {
-            Toast.makeText(
-                this@MainActivity,
-                R.string.incomplete_expression,
-                Toast.LENGTH_SHORT
-            ).show()
-            return
-        }
+    override fun onViewUpdated(newExpression: Expression) {
+        binding.textView.text = newExpression.toString()
+        expression = newExpression
+    }
 
-        expression = Expression.EMPTY + result
-        binding.textView.text = result.toString()
+    override fun onExpressionIncomplete() {
+        Toast.makeText(
+            this@MainActivity,
+            R.string.incomplete_expression,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
