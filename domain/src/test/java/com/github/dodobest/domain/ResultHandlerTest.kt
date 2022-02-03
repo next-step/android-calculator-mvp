b55c4ad028395b73b3a6668572168a165aeb9989
DoDoBest <av449@naver.com>
2022-02-03 09:24:30 +0900

    test: ResultHandler 단위 테스트 작성

---
+++ domain/src/test/java/com/github/dodobest/domain/ResultHandlerTest.kt
@@ -0,0 +1,26 @@
+package com.github.dodobest.domain
+
+import com.google.common.truth.Truth
+import org.junit.Before
+import org.junit.Test
+
+class ResultHandlerTest {
+    private lateinit var resultHandler: ResultHandler
+
+    @Before
+    fun setUp() {
+        resultHandler = ResultHandler()
+    }
+
+    @Test
+    fun `값을 입력하면 배열에 추가된다`() {
+        // when
+        resultHandler.add("3+3", "5")
+        resultHandler.add("1+1", "2")
+        val actual: ArrayList<Result> = resultHandler.getResultList()
+
+        // then
+        Truth.assertThat(actual).isEqualTo(arrayListOf(Result("3+3", "5")
+            , Result("1+1", "2")))
+    }
+}
