ab4ca7eb0beb65f59122373ba9c3308c32d59ebf
Woody <woody.lee@balancehero.com>
2022-02-04 13:54:20 +0900

    refactor: ViewBinding으로 변경

--- app/src/main/java/edu/nextstep/camp/calculator/MainHistoryAdapter.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainHistoryAdapter.kt
@@ -1,31 +1,27 @@
 package edu.nextstep.camp.calculator
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import edu.nextstep.camp.calculator.databinding.ItemResultBinding
 import edu.nextstep.camp.calculator.domain.Record
 
 class MainHistoryAdapter : ListAdapter<Record, MainHistoryAdapter.ViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_result, parent, false))
+        return ViewHolder(ItemResultBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val tvExpression = itemView.findViewById<TextView>(R.id.tv_expression)
-        private val tvResult = itemView.findViewById<TextView>(R.id.tv_result)
-
+    class ViewHolder(private val itemResultBinding: ItemResultBinding) : RecyclerView.ViewHolder(itemResultBinding.root) {
         fun bind(record: Record) {
-            tvExpression.text = record.expression
-            tvResult.text = record.result
+            itemResultBinding.tvExpression.text = record.expression
+            itemResultBinding.tvResult.text = record.result
         }
     }
 
