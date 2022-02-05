2066a9fcc5eb429af8a5e80f6ef3e5d332e67136
LeeChungWoo <skcjddn@gmail.com>
2022-02-06 00:19:40 +0900

    refactor: toast,calculate 함수 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -1,18 +1,19 @@
 package edu.nextstep.camp.calculator
 
-import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
 
 interface MainContract {
-    interface View{
+    interface View {
 
-        fun refreshCount(expression: Expression)
+        fun refreshCount(text: String)
+        fun showToast(message: Int)
     }
 
-    interface Presenter{
+    interface Presenter {
 
         fun inputNumber(value: Int)
         fun inputOperator(operator: Operator)
         fun removeLast()
+        fun calculate()
     }
 }
