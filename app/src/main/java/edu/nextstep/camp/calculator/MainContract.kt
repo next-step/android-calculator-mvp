e623d48e9db691843359990c7aaec70f0147b83c
LeeChungWoo <skcjddn@gmail.com>
2022-02-05 23:53:42 +0900

    refactor: View,Presenter 인터페이스 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -1,4 +1,18 @@
 package edu.nextstep.camp.calculator
 
+import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.Operator
+
 interface MainContract {
+    interface View{
+
+        fun refreshCount(expression: Expression)
+    }
+
+    interface Presenter{
+
+        fun inputNumber(value: Int)
+        fun inputOperator(operator: Operator)
+        fun removeLast()
+    }
 }
