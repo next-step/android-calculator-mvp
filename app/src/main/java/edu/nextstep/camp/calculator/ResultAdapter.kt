91a1fadbd0c81f50aa8d3f3050c84c7208a4dcce
DoDoBest <av449@naver.com>
2022-02-06 21:04:55 +0900

    refactor: ViewHolder가 binding을 이용하도록 수정

--- app/src/main/java/edu/nextstep/camp/calculator/ResultAdapter.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/ResultAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.github.dodobest.domain.ResultHandler
+import edu.nextstep.camp.calculator.databinding.ItemResultBinding
 
 class ResultAdapter(private val resultHandler: ResultHandler) : RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemResultBinding = ItemResultBinding.inflate(LayoutInflater.from(parent.context))
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_result, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- app/src/main/java/edu/nextstep/camp/calculator/ViewHolder.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/ViewHolder.kt
@@ -3,8 +3,9 @@ package edu.nextstep.camp.calculator
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import edu.nextstep.camp.calculator.databinding.ItemResultBinding
 
-class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val expression: TextView = view.findViewById(R.id.tv_expression)
-    val result: TextView = view.findViewById(R.id.tv_result)
+class ViewHolder(view: View, binding: ItemResultBinding) : RecyclerView.ViewHolder(view) {
+    val expression: TextView = binding.tvExpression
+    val result: TextView = binding.tvResult
 }
