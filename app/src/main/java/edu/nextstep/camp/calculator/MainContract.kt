1e22fbc6a6a0be85cb2bd71a5bd3c8a72b1f8ae6
LeeChungWoo <skcjddn@gmail.com>
2022-02-08 23:42:35 +0900

    refactor: 함수명,파라미터 전체적으로 수정

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -1,18 +1,20 @@
 package edu.nextstep.camp.calculator
 
+import edu.nextstep.camp.calculator.domain.Expression
 import edu.nextstep.camp.calculator.domain.Operator
 
 interface MainContract {
     interface View {
+        var presenter:Presenter
 
-        fun refreshExpression(text: String)
-        fun showToast(message: Int)
+        fun showExpression(expression: Expression)
+        fun showResult(result: Int)
+        fun showIncompleteExpressionError()
     }
 
     interface Presenter {
-
-        fun inputNumber(value: Int)
-        fun inputOperator(operator: Operator)
+        fun addToExpression(operand: Int)
+        fun addToExpression(operator: Operator)
         fun removeLast()
         fun calculate()
     }
