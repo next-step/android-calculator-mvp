11998f6a49d4345e1f2a5a27175a3d4d8fa2f63b
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-09 00:20:38 +0900

    feat(step3) : 비어있지 않은 계산기록에 추가하는 테스트 작성
    - 계산 기록이 비어있지 않은 경우 계산 기록을 추가해도 뒤에 추가되어 저장되는 테스트 작성

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationMemoryTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationMemoryTest.kt
@@ -17,4 +17,21 @@ class CalculationMemoryTest {
         assertThat(calculationMemory.records.lastOrNull()?.expression).isEqualTo(expectedExpression)
         assertThat(calculationMemory.records.lastOrNull()?.result).isEqualTo(3)
     }
+
+    @Test
+    fun `비어있지 않은 계산 기록에 기록을 추가하면 기록이 추가되어 저장된다`() {
+        // given :
+        val oldExpression = Expression(listOf(1, Operator.Plus, 2))
+        val oldExpressionResult = 3
+        val calculationMemory =
+            CalculationMemory(CalculationMemory.Record(oldExpression, oldExpressionResult))
+        val newExpression = Expression(listOf(2, Operator.Minus, 1))
+        val newExpressionResult = 1
+        // when :
+        calculationMemory.addRecord(newExpression, newExpressionResult)
+        // then :
+        val expectedExpression = Expression(listOf(2, Operator.Minus, 1))
+        assertThat(calculationMemory.records.lastOrNull()?.expression).isEqualTo(expectedExpression)
+        assertThat(calculationMemory.records.lastOrNull()?.result).isEqualTo(1)
+    }
 }
