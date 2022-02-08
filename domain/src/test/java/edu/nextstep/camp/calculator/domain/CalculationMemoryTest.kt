41eaa31a7b017d515ecc9c0bc894faab2ca309c4
jinsu4755 <parkjinsu4755@gmail.com>
2022-02-09 00:39:43 +0900

    feat(step3) : 계산기록에 추가하는 테스트 수정
    - 계산 기록 추가는 size 만 확인하는 것으로 충분하다 생각하여 변경하였음
    - 계산 기록의 추가가 잘 되었는지 내부 값을 검사하는 테스트로 변경

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationMemoryTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationMemoryTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class CalculationMemoryTest {
     @Test
-    fun `메모리에 계산 기록을 추가하면 기록이 추가되어 저장된다`() {
+    fun `메모리에 계산 기록을 추가하면 기록이 추가된다`() {
         // given :
         val calculationMemory = CalculationMemory()
         val newExpression = Expression(listOf(1, Operator.Plus, 2))
@@ -13,27 +13,23 @@ class CalculationMemoryTest {
         // when :
         calculationMemory.addRecord(newExpression, newExpressionResult)
         // then :
-        val expectedExpression = Expression(listOf(1, Operator.Plus, 2))
-        assertThat(calculationMemory.records.lastOrNull()?.expression).isEqualTo(expectedExpression)
-        assertThat(calculationMemory.records.lastOrNull()?.result).isEqualTo(3)
         assertThat(calculationMemory.records.size).isEqualTo(1)
     }
 
     @Test
-    fun `비어있지 않은 계산 기록에 기록을 추가하면 기록이 추가되어 저장된다`() {
+    fun `계산 기록에 기록을 추가하면 새로운 기록이 추가되어 저장된다`() {
         // given :
         val oldExpression = Expression(listOf(1, Operator.Plus, 2))
         val oldExpressionResult = 3
         val oldRecord = CalculationMemory.Record(oldExpression, oldExpressionResult)
+
         val calculationMemory = CalculationMemory(oldRecord)
         // when :
         val newExpression = Expression(listOf(2, Operator.Minus, 1))
         val newExpressionResult = 1
         calculationMemory.addRecord(newExpression, newExpressionResult)
         // then :
-        val expectedExpression = Expression(listOf(2, Operator.Minus, 1))
-        assertThat(calculationMemory.records.lastOrNull()?.expression).isEqualTo(expectedExpression)
+        assertThat(calculationMemory.records.lastOrNull()?.expression).isEqualTo(newExpression)
         assertThat(calculationMemory.records.lastOrNull()?.result).isEqualTo(1)
-        assertThat(calculationMemory.records.size).isEqualTo(2)
     }
 }
