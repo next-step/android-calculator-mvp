050c48c4769cfa90911a60292aa8418ec7eddfc8
jennkimm <seohkim@student.42seoul.kr>
2022-02-13 17:41:44 +0900

    refactor: MainPresenterTest slot 제거 및 주석 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -4,9 +4,7 @@ import edu.nextstep.camp.calculator.domain.Operator
 
 interface MainContract {
     interface View : BaseView<Presenter> {
-        // TextView 에 Expression 데이터를 보여준다
         fun showExpression(expression: String)
-        // 에러 메시지 보여준다
         fun showIncompleteExpressionError()
     }
 
