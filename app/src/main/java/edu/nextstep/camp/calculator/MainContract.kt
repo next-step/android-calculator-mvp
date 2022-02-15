bc721d3d31964cdd830ac2617004120f543ce96a
LeeChungWoo <skcjddn@gmail.com>
2022-02-15 22:20:13 +0900

    add: 계산기록 관련 presenter,viwe 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -1,15 +1,17 @@
 package edu.nextstep.camp.calculator
 
 import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.History
 import edu.nextstep.camp.calculator.domain.Operator
 
 interface MainContract {
     interface View {
-        var presenter:Presenter
+        var presenter: Presenter
 
         fun showExpression(expression: Expression)
         fun showResult(result: Int)
         fun showIncompleteExpressionError()
+        fun showHistory(history: History)
     }
 
     interface Presenter {
@@ -17,5 +19,6 @@ interface MainContract {
         fun addToExpression(operator: Operator)
         fun removeLast()
         fun calculate()
+        fun addHistoryResult()
     }
 }
