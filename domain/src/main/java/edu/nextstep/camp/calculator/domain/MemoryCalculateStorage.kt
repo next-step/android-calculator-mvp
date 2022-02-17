91a5ff3b94a6fd657d57b80daf0a924ecd13cc67
junpil Park <iamfeel!2@gmail.com>
2022-02-17 10:27:00 +0900

    refactor: 방어적인 복사 추가

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/MemoryCalculateStorage.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/MemoryCalculateStorage.kt
@@ -3,7 +3,7 @@ package edu.nextstep.camp.calculator.domain
 class MemoryCalculateStorage(history: List<HistoryItem> = emptyList()) : CalculateStorage {
     private val _history: MutableList<HistoryItem> = history.toMutableList()
     override val history: List<HistoryItem>
-        get() = _history
+        get() = _history.toList()
 
     override fun save(formula: Expression, result: Expression) {
         _history.add(HistoryItem(formula, result))
