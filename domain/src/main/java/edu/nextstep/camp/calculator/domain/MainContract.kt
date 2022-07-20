ceff16e9490519ba0d1a6d9e2970a30a67f13eb2
greedy0110 <greedy0110@gmail.com>
2022-07-20 21:58:11 +0900

    MainPresenter 테스트 작성

---
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/MainContract.kt
@@ -0,0 +1,50 @@
+package edu.nextstep.camp.calculator.domain
+
+interface MainContract {
+    interface View {
+        fun showExpression(expression: Expression)
+        fun showCalculationFailMessage()
+    }
+
+    interface Presenter {
+        fun addToExpression(operand: Int)
+        fun addToExpression(operator: Operator)
+        fun delete()
+        fun calculate()
+    }
+}
+
+class MainPresenter(
+    private val view: MainContract.View,
+) : MainContract.Presenter {
+
+    private val calculator = Calculator()
+    private var expression = Expression.EMPTY
+
+    override fun addToExpression(operand: Int) {
+        expression += operand
+        view.showExpression(expression)
+    }
+
+    override fun addToExpression(operator: Operator) {
+        expression += operator
+        view.showExpression(expression)
+    }
+
+    override fun delete() {
+        expression.removeLast()
+        view.showExpression(expression)
+    }
+
+    override fun calculate() {
+        val result = calculator.calculate(expression.toString())
+
+        if (result == null) {
+            view.showCalculationFailMessage()
+            return
+        }
+
+        expression = Expression.newInstance(result)
+        view.showExpression(expression)
+    }
+}
---
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/MainPresenterTest.kt
@@ -0,0 +1,90 @@
+package edu.nextstep.camp.calculator.domain
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class MainPresenterTest {
+
+    private lateinit var view: MainContract.View
+    private lateinit var presenter: MainPresenter
+
+    @BeforeEach
+    fun setup() {
+        view = mockk(relaxUnitFun = true)
+        presenter = MainPresenter(view)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+    fun `display operand added expression when add operand on empty expression`(operand: Int) {
+        // when
+        presenter.addToExpression(operand)
+
+        // then
+        val expected = Expression.EMPTY + operand
+        verify { view.showExpression(expected) }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Operator::class)
+    fun `display operator added expression when add operator on empty expression`(operator: Operator) {
+        // when
+        presenter.addToExpression(operator)
+
+        // then
+        val expected = Expression.EMPTY + operator
+        verify { view.showExpression(expected) }
+    }
+
+    @Test
+    fun `display last piece deleted expression when delete`() {
+        // given "12 +"
+        presenter.addToExpression(1)
+        presenter.addToExpression(2)
+        presenter.addToExpression(Operator.Plus)
+
+        // when
+        presenter.delete()
+
+        // then
+        val expected = Expression.EMPTY + 1 + 2
+        verify { view.showExpression(expected) }
+    }
+
+    @Test
+    fun `display final answer when calculate`() {
+        // given "1 + 3 * 6"
+        presenter.addToExpression(1)
+        presenter.addToExpression(Operator.Plus)
+        presenter.addToExpression(3)
+        presenter.addToExpression(Operator.Multiply)
+        presenter.addToExpression(6)
+
+        // when
+        presenter.calculate()
+
+        // then
+        val expected = Expression.newInstance(24)
+        verify { view.showExpression(expected) }
+    }
+
+    @Test
+    fun `show fail message and do nothing with display when calculate on uncompleted expression`() {
+        // given "1 *"
+        presenter.addToExpression(1)
+        presenter.addToExpression(Operator.Multiply)
+        verify(exactly = 2) { view.showExpression(any()) }
+
+        // when
+        presenter.calculate()
+
+        // then
+        verify { view.showCalculationFailMessage() }
+        verify(exactly = 2) { view.showExpression(any()) }
+    }
+}
