62774249c9e7c220a42958fda5ed7260dcc120fe
AnKyungMoo <sodp2@naver.com>
2022-07-23 01:26:56 +0900

    test: 연산자 입력 테스트 추가

--- app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
@@ -1,6 +1,7 @@
 package edu.nextstep.camp.calculator
 
 import com.google.common.truth.Truth.assertThat
+import edu.nextstep.camp.calculator.domain.Operator
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
@@ -30,6 +31,22 @@ class MainPresenterTest {
         // then
         val actual = expressionSlot.captured
         assertThat(actual).isEqualTo("1")
+        verify { view.showExpression(actual) }
+    }
+
+    @Test
+    fun `연산자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
+        // given
+        val expressionSlot = slot<String>()
+        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+
+        // when
+        presenter.input(1)
+        presenter.input(Operator.Plus)
+
+        // then
+        val actual = expressionSlot.captured
+        assertThat(actual).isEqualTo("1 +")
         verify { view.showExpression(actual) }
     }
 }
