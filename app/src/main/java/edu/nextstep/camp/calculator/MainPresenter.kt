839bae59714a184ec21b605bf380aeebe94ccb12
AnKyungMoo <sodp2@naver.com>
2022-07-22 21:27:37 +0900

    refactor: 계산기 코드를 mvp로 변경

---
+++ app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
@@ -0,0 +1,40 @@
+package edu.nextstep.camp.calculator
+
+import edu.nextstep.camp.calculator.domain.Calculator
+import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.Operator
+
+class MainPresenter(
+    private val view: MainContract.View
+) : MainContract.Presenter {
+    private val calculator = Calculator()
+    private var expression = Expression.EMPTY
+
+    init {
+        view.showExpression(expression.toString())
+    }
+
+    override fun input(operand: Int) {
+        expression += operand
+        view.showExpression(expression.toString())
+    }
+
+    override fun input(operator: Operator) {
+        expression += operator
+        view.showExpression(expression.toString())
+    }
+
+    override fun removeLast() {
+        expression = expression.removeLast()
+        view.showExpression(expression.toString())
+    }
+
+    override fun calculate(): Int? {
+        return calculator.calculate(expression.toString())
+    }
+
+    override fun initExpression(result : Int) {
+        expression = Expression.EMPTY + result
+        view.showExpression(expression.toString())
+    }
+}
