18f6e6b4e15861484784b73b0d625612ce5d26fe
AnKyungMoo <sodp2@naver.com>
2022-07-23 00:15:16 +0900

    test: 숫자 입력 테스트 추가

---
+++ app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
@@ -0,0 +1,35 @@
+package edu.nextstep.camp.calculator
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+
+class MainPresenterTest {
+    private lateinit var presenter: MainPresenter
+    private lateinit var view: MainContract.View
+
+    @Before
+    fun setUp() {
+        view = mockk()
+        presenter = MainPresenter(view)
+    }
+
+    @Test
+    fun `숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
+        // given
+        val expressionSlot = slot<String>()
+        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+
+        // when
+        presenter.input(1)
+
+        // then
+        val actual = expressionSlot.captured
+        assertThat(actual).isEqualTo("1")
+        verify { view.showExpression(actual) }
+    }
+}
