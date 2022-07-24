ac15265e84b2fc64c131d68ff22ca511335192bf
yeonjae22 <yeonberry22@gmail.com>
2022-07-24 23:47:57 +0900

    feat: 계산기 (MVP)

---
+++ app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
@@ -0,0 +1,38 @@
+package edu.nextstep.camp.calculator
+
+import edu.nextstep.camp.calculator.domain.Calculator
+import edu.nextstep.camp.calculator.domain.Expression
+import edu.nextstep.camp.calculator.domain.Operator
+
+class MainPresenter(
+    private val view: MainContract.View
+) : MainContract.Presenter {
+
+    private val calculator = Calculator()
+    private var expression = Expression.EMPTY
+
+    override fun addToExpression(operand: Int) {
+        expression += operand
+        view.showExpression(expression)
+    }
+
+    override fun addToExpression(operator: Operator) {
+        expression += operator
+        view.showExpression(expression)
+    }
+
+    override fun removeLast() {
+        expression = expression.removeLast()
+        view.showExpression(expression)
+    }
+
+    override fun calculate() {
+        val result = calculator.calculate(expression.toString())
+        if (result == null) {
+            view.showIncompleteExpressionToast()
+            return
+        }
+        expression = Expression.EMPTY + result
+        view.showExpression(expression)
+    }
+}
