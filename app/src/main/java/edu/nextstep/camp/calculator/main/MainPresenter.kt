7ad5d14042bb6564bf7411d3c0a6c84bb1792080
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-07-24 17:07:33 +0900

    removeLast 작동할 수 있도록 수정

--- app/src/main/java/edu/nextstep/camp/calculator/main/MainPresenter.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/main/MainPresenter.kt
@@ -21,7 +21,7 @@ class MainPresenter(
     }
 
     override fun removeLast() {
-        expression.removeLast()
+        expression = expression.removeLast()
         view.showExpression(expression.toString())
     }
 
