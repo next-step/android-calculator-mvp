9fa36318d3f7557fa6f24c27343bad1d41e247a2
Suhyeon Kim <wisemuji@gmail.com>
2022-07-27 03:10:21 +0900

    refactor: MainPresenter의 중복된 로직을 함수로 분리

--- app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainPresenter.kt
@@ -12,23 +12,14 @@ class MainPresenter(
 
     private var state: StringExpressionState = initialState
 
-    override fun addElement(operator: Operator) {
-        state = state
-            .addElement(operator)
-            .also(view::setExpression)
-    }
+    override fun addElement(operator: Operator) =
+        updateViewState(state.addElement(operator))
 
-    override fun addElement(operand: Operand) {
-        state = state
-            .addElement(operand)
-            .also(view::setExpression)
-    }
+    override fun addElement(operand: Operand) =
+        updateViewState(state.addElement(operand))
 
-    override fun removeElement() {
-        state = state
-            .removeElement()
-            .also(view::setExpression)
-    }
+    override fun removeElement() =
+        updateViewState(state.removeElement())
 
     override fun calculate() {
         runCatching {
@@ -38,5 +29,9 @@ class MainPresenter(
             .onFailure {
                 view.calculationFailed()
             }
+    }
+
+    private fun updateViewState(state: StringExpressionState) {
+        this.state = state.also(view::setExpression)
     }
 }
