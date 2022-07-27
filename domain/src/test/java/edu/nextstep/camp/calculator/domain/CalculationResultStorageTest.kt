cc2349d6aabf48a2e8fdea7e4a08a10de51b16d8
sinoso <tpgus123123@naver.com>
2022-07-27 20:23:49 +0900

    refactor : CalculationResultStorageTest 반복되는 given을 BeforeEach를 사용한 형태로 수정

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationResultStorageTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculationResultStorageTest.kt
@@ -1,15 +1,20 @@
 package edu.nextstep.camp.calculator.domain
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class CalculationResultStorageTest {
+    private lateinit var storage: CalculationResultStorage
+
+    @BeforeEach
+    fun setUp() {
+        storage = CalculationResultStorage()
+    }
+
     @Test
     fun `계산기록 저장소에 추가된 계산기록이 없을 때 계산 결과를 가져오면 빈 list를 반환한다`() {
-        // given 추가된 계산기록이 없을 때
-        val storage = CalculationResultStorage()
-
-        // when 계산기록을 요청하면
+        // when 추가된 계산기록이 없을 때 계산기록을 요청하면
         val resultList = storage.getResultsAsList()
 
         // then 빈 list를 반환한다.
@@ -18,10 +23,7 @@ class CalculationResultStorageTest {
 
     @Test
     fun `계산기록 저장소에 계산기록을 추가 하면 추가된 계산 기록을 받아 올 수 있다`() {
-        // given 계산기록 저장소에
-        var storage = CalculationResultStorage()
-
-        // when 계산기록을 추가 하면
+        // when 계산기록 저장소에 계산기록을 추가 하면
         val expectedExpression = Expression.EMPTY + 1 + Operator.Plus + 1
         val expected = CalculationResult(expectedExpression, 2)
         storage += expected
@@ -34,10 +36,7 @@ class CalculationResultStorageTest {
 
     @Test
     fun `계산기록 저장소에 계산기록을 2개 이상 추가 하면, 추가된 순서대로 저장된 계산 기록을 받아 올 수 있다`() {
-        // given 계산기록 저장소에
-        var storage = CalculationResultStorage()
-
-        // when 계산기록을 2개 이상 추가 하면
+        // when 계산기록 저장소에 계산기록을 2개 이상 추가 하면
         val firstExpectedExpression = Expression.EMPTY + 1 + Operator.Plus + 1
         val firstExpected = CalculationResult(firstExpectedExpression, 2)
         storage += firstExpected
