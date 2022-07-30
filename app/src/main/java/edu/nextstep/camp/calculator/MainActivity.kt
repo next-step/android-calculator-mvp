17868e7932c40a9e9f9c6a3de6b22aa69c9c2df9
박찬준 <chanjun.park@navercorp.com>
2022-07-30 15:14:36 +0900

    계산기 코드 mvp 패턴으로 변경

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -4,88 +4,49 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
+import edu.nextstep.camp.common.UiText
 import edu.nextstep.camp.domain.Calculator
 import edu.nextstep.camp.domain.Expression
 import edu.nextstep.camp.domain.Operator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var binding: ActivityMainBinding
-    private val calculator = Calculator()
-    private var expression = Expression.EMPTY
+    private val presenter = MainPresenter(this, Calculator())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener {
-            expression += 0
-            binding.textView.text = expression.toString()
-        }
-        binding.button1.setOnClickListener {
-            expression += 1
-            binding.textView.text = expression.toString()
-        }
-        binding.button2.setOnClickListener {
-            expression += 2
-            binding.textView.text = expression.toString()
-        }
-        binding.button3.setOnClickListener {
-            expression += 3
-            binding.textView.text = expression.toString()
-        }
-        binding.button4.setOnClickListener {
-            expression += 4
-            binding.textView.text = expression.toString()
-        }
-        binding.button5.setOnClickListener {
-            expression += 5
-            binding.textView.text = expression.toString()
-        }
-        binding.button6.setOnClickListener {
-            expression += 6
-            binding.textView.text = expression.toString()
-        }
-        binding.button7.setOnClickListener {
-            expression += 7
-            binding.textView.text = expression.toString()
-        }
-        binding.button8.setOnClickListener {
-            expression += 8
-            binding.textView.text = expression.toString()
-        }
-        binding.button9.setOnClickListener {
-            expression += 9
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonPlus.setOnClickListener {
-            expression += Operator.Plus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMinus.setOnClickListener {
-            expression += Operator.Minus
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonMultiply.setOnClickListener {
-            expression += Operator.Multiply
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDivide.setOnClickListener {
-            expression += Operator.Divide
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonDelete.setOnClickListener {
-            expression = expression.removeLast()
-            binding.textView.text = expression.toString()
-        }
-        binding.buttonEquals.setOnClickListener {
-            val result = calculator.calculate(expression.toString())
-            if (result == null) {
-                Toast.makeText(this, R.string.incomplete_expression, Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-            expression = Expression.EMPTY + result
-            binding.textView.text = result.toString()
-        }
+        binding.button0.setOnClickListener { presenter.addNumberToExpression(0) }
+        binding.button1.setOnClickListener { presenter.addNumberToExpression(1) }
+        binding.button2.setOnClickListener { presenter.addNumberToExpression(2) }
+        binding.button3.setOnClickListener { presenter.addNumberToExpression(3) }
+        binding.button4.setOnClickListener { presenter.addNumberToExpression(4) }
+        binding.button5.setOnClickListener { presenter.addNumberToExpression(5) }
+        binding.button6.setOnClickListener { presenter.addNumberToExpression(6) }
+        binding.button7.setOnClickListener { presenter.addNumberToExpression(7) }
+        binding.button8.setOnClickListener { presenter.addNumberToExpression(8) }
+        binding.button9.setOnClickListener { presenter.addNumberToExpression(9) }
+
+        binding.buttonPlus.setOnClickListener { presenter.addOperatorToExpression(Operator.Plus) }
+        binding.buttonMinus.setOnClickListener { presenter.addOperatorToExpression(Operator.Minus) }
+        binding.buttonMultiply.setOnClickListener { presenter.addOperatorToExpression(Operator.Multiply) }
+        binding.buttonDivide.setOnClickListener { presenter.addOperatorToExpression(Operator.Divide) }
+
+        binding.buttonDelete.setOnClickListener { presenter.removeLastToken() }
+        binding.buttonEquals.setOnClickListener { presenter.calculateCurrentExpression() }
+    }
+
+    override fun showExpression(expression: Expression) {
+        binding.textView.text = expression.toString()
+    }
+
+    override fun showResult(result: Int) {
+        binding.textView.text = result.toString()
+    }
+
+    override fun showErrorMessage(uiText: UiText) {
+        Toast.makeText(this, uiText.asString(this), Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/edu/nextstep/camp/calculator/MainContract.kt
@@ -0,0 +1,20 @@
+package edu.nextstep.camp.calculator
+
+import edu.nextstep.camp.common.UiText
+import edu.nextstep.camp.domain.Expression
+import edu.nextstep.camp.domain.Operator
+
+interface MainContract {
+	interface View {
+		fun showExpression(expression: Expression)
+		fun showResult(result: Int)
+		fun showErrorMessage(uiText: UiText)
+	}
+
+	interface Presenter {
+		fun addNumberToExpression(number: Int)
+		fun addOperatorToExpression(operator: Operator)
+		fun removeLastToken()
+		fun calculateCurrentExpression()
+	}
+}
