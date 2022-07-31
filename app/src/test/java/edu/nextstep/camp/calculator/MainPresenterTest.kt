4f6de04f5301084a2416d82bd0cdb7ae28a0513b
박찬준 <pcj01142@gmail.com>
2022-07-31 22:57:37 +0900

    stubbing 이 필요없는 부분 mockk just run 활용

--- app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/MainPresenterTest.kt
@@ -27,7 +27,7 @@ class MainPresenterTest {
     fun `숫자가 입력되면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.addNumberToExpression(1)
@@ -42,7 +42,7 @@ class MainPresenterTest {
     fun `숫자가 입력된 상태에서 다른 숫자를 입력하면 수식에 추가되고 변경된 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
         presenter.addNumberToExpression(1)
 
         // when
@@ -58,7 +58,7 @@ class MainPresenterTest {
     fun `숫자가 입력된 상태에서 + 연산자를 입력하면 수식에 추가되고 숫자 + 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
         presenter.addNumberToExpression(1)
 
         // when
@@ -74,7 +74,7 @@ class MainPresenterTest {
     fun `숫자가 입력된 상태에서 - 연산자를 입력하면 수식에 추가되고 숫자 - 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
         presenter.addNumberToExpression(1)
 
         // when
@@ -90,7 +90,7 @@ class MainPresenterTest {
     fun `숫자가 입력된 상태에서 × 연산자를 입력하면 수식에 추가되고 숫자 × 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
         presenter.addNumberToExpression(1)
 
         // when
@@ -106,7 +106,7 @@ class MainPresenterTest {
     fun `숫자가 입력된 상태에서 ÷ 연산자를 입력하면 수식에 추가되고 숫자 ÷ 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
         presenter.addNumberToExpression(1)
 
         // when
@@ -122,7 +122,7 @@ class MainPresenterTest {
     fun `아무런 입력이 없는 상태에서 + 연산자를 입력하면 수식에 추가되지 않고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.addOperatorToExpression(Operator.Plus)
@@ -137,7 +137,7 @@ class MainPresenterTest {
     fun `아무런 입력이 없는 상태에서 - 연산자를 입력하면 수식에 추가되지 않고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.addOperatorToExpression(Operator.Minus)
@@ -152,7 +152,7 @@ class MainPresenterTest {
     fun `아무런 입력이 없는 상태에서 × 연산자를 입력하면 수식에 추가되지 않고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.addOperatorToExpression(Operator.Multiply)
@@ -167,7 +167,7 @@ class MainPresenterTest {
     fun `아무런 입력이 없는 상태에서 ÷ 연산자를 입력하면 수식에 추가되지 않고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.addOperatorToExpression(Operator.Divide)
@@ -182,7 +182,7 @@ class MainPresenterTest {
     fun `아무런 입력이 없는 상태에서 지우기를 하면 수식에 변화가 없고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) } just runs
 
         // when
         presenter.removeLastToken()
@@ -198,7 +198,7 @@ class MainPresenterTest {
     fun `숫자, + 연산자가 입력된 상태에서 + 연산자를 입력하는 경우 연산자가 변경되고 숫자 + 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Plus)
 
@@ -215,7 +215,7 @@ class MainPresenterTest {
     fun `숫자, + 연산자가 입력된 상태에서 - 연산자를 입력하는 경우 연산자가 변경되고 숫자 - 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Plus)
 
@@ -232,7 +232,7 @@ class MainPresenterTest {
     fun `숫자, + 연산자가 입력된 상태에서 × 연산자를 입력하는 경우 연산자가 변경되고 숫자 × 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Plus)
 
@@ -249,7 +249,7 @@ class MainPresenterTest {
     fun `숫자, + 연산자가 입력된 상태에서 ÷ 연산자를 입력하는 경우 연산자가 변경되고 숫자 ÷ 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Plus)
 
@@ -266,7 +266,7 @@ class MainPresenterTest {
     fun `숫자, - 연산자가 입력된 상태에서 + 연산자를 입력하는 경우 연산자가 변경되고 변경된 숫자 + 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Minus)
 
@@ -283,7 +283,7 @@ class MainPresenterTest {
     fun `숫자, - 연산자가 입력된 상태에서 - 연산자를 입력하는 경우 연산자가 변경되고 숫자 - 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Minus)
 
@@ -300,7 +300,7 @@ class MainPresenterTest {
     fun `숫자, - 연산자가 입력된 상태에서 × 연산자를 입력하는 경우 연산자가 변경되고 숫자 × 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Minus)
 
@@ -317,7 +317,7 @@ class MainPresenterTest {
     fun `숫자, - 연산자가 입력된 상태에서 ÷ 연산자를 입력하는 경우 연산자가 변경되고 숫자 ÷ 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Minus)
 
@@ -334,7 +334,7 @@ class MainPresenterTest {
     fun `숫자, × 연산자가 입력된 상태에서 + 연산자를 입력하는 경우 연산자가 변경되고 숫자 + 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Multiply)
 
@@ -351,7 +351,7 @@ class MainPresenterTest {
     fun `숫자, × 연산자가 입력된 상태에서 - 연산자를 입력하는 경우 연산자가 변경되고 숫자 - 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Multiply)
 
@@ -368,7 +368,7 @@ class MainPresenterTest {
     fun `숫자, × 연산자가 입력된 상태에서 × 연산자를 입력하는 경우 연산자가 변경되고 숫자 × 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Multiply)
 
@@ -385,7 +385,7 @@ class MainPresenterTest {
     fun `숫자, × 연산자가 입력된 상태에서 ÷ 연산자를 입력하는 경우 연산자가 변경되고 숫자 ÷ 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Multiply)
 
@@ -402,7 +402,7 @@ class MainPresenterTest {
     fun `숫자, ÷ 연산자가 입력된 상태에서 + 연산자를 입력하는 경우 연산자가 변경되고 숫자 + 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Divide)
 
@@ -419,7 +419,7 @@ class MainPresenterTest {
     fun `숫자, ÷ 연산자가 입력된 상태에서 - 연산자를 입력하는 경우 연산자가 변경되고 숫자 - 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Divide)
 
@@ -436,7 +436,7 @@ class MainPresenterTest {
     fun `숫자, ÷ 연산자가 입력된 상태에서 × 연산자를 입력하는 경우 연산자가 변경되고 숫자 × 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Divide)
 
@@ -453,7 +453,7 @@ class MainPresenterTest {
     fun `숫자, ÷ 연산자가 입력된 상태에서 ÷ 연산자를 입력하는 경우 연산자가 변경되고 숫자 ÷ 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Divide)
 
@@ -470,7 +470,7 @@ class MainPresenterTest {
     fun `숫자, + 연산자가 입력된 상태에서 숫자를 입력하는 경우 연산식에 숫자가 추가되고 숫자 + 숫자 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Plus)
 
@@ -487,7 +487,7 @@ class MainPresenterTest {
     fun `숫자, - 연산자가 입력된 상태에서 숫자를 입력하는 경우 연산식에 숫자가 추가되고 숫자 - 숫자 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Minus)
 
@@ -504,7 +504,7 @@ class MainPresenterTest {
     fun `숫자, × 연산자가 입력된 상태에서 숫자를 입력하는 경우 연산식에 숫자가 추가되고 숫자 × 숫자 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Multiply)
 
@@ -521,7 +521,7 @@ class MainPresenterTest {
     fun `숫자, ÷ 연산자가 입력된 상태에서 숫자를 입력하는 경우 연산식에 숫자가 추가되고 숫자 ÷ 숫자 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(1)
         presenter.addOperatorToExpression(Operator.Divide)
 
@@ -538,7 +538,7 @@ class MainPresenterTest {
     fun `32 + 1 수식이 입력되었을때 지우기를 수행하면 1이 제거되고 32 + 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
         presenter.addOperatorToExpression(Operator.Plus)
@@ -557,7 +557,7 @@ class MainPresenterTest {
     fun `32 + 수식이 입력되었을때 지우기를 수행하면 +가 제거되고 32 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
         presenter.addOperatorToExpression(Operator.Plus)
@@ -575,7 +575,7 @@ class MainPresenterTest {
     fun `32 수식이 입력되었을때 지우기를 수행하면 2가 제거되고 3 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
 
@@ -592,7 +592,7 @@ class MainPresenterTest {
     fun `3 수식이 입력되었을때 지우기를 수행하면 3이 제거되고 빈 수식을 보여줘야 한다`() {
         // given
         val expressionSlot = slot<Expression>()
-        every { view.showExpression(capture(expressionSlot)) } answers { nothing }
+        every { view.showExpression(capture(expressionSlot)) }  just runs
         presenter.addNumberToExpression(3)
 
         // when
@@ -608,8 +608,8 @@ class MainPresenterTest {
     fun `32 + 1 수식이 입력되었을때 연산을 수행하면 계산 결과인 33을 화면에 보여줘야 한다`() {
         // given
         val resultSlot = slot<Int>()
-        every { view.showResult(capture(resultSlot)) } answers { nothing }
-        every { view.showExpression(any()) } answers { nothing }
+        every { view.showResult(capture(resultSlot)) }  just runs
+        every { view.showExpression(any()) }  just runs
 
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
@@ -629,8 +629,8 @@ class MainPresenterTest {
     fun `32 + 수식이 입력되었을때 연산을 수행하면 유효하지 않은 수식에 대한 에러 메시지를 보여줘야 한다`() {
         // given
         val errorExceptionSlot = slot<Exception>()
-        every { view.showErrorMessage(capture(errorExceptionSlot)) } answers { nothing }
-        every { view.showExpression(any()) } answers { nothing }
+        every { view.showErrorMessage(capture(errorExceptionSlot)) }  just runs
+        every { view.showExpression(any()) }  just runs
 
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
@@ -648,8 +648,8 @@ class MainPresenterTest {
     @Test
     fun `32 + 1 수식이 입력되었을때 연산을 수행하면 연산식과 계산 결과인 33을 연산 기록에 저장해야 한다`() {
         // given
-        every { view.showResult(any()) } answers { nothing }
-        every { view.showExpression(any()) } answers { nothing }
+        every { view.showResult(any()) }  just runs
+        every { view.showExpression(any()) }  just runs
 
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
@@ -669,9 +669,9 @@ class MainPresenterTest {
     fun `계산 UI가 보여지는 상태에서 시계 버튼을 눌러 UI 를 변경하면 계산 기록을 보여주는 UI 로 변경되어야 한다`() {
         // given
         val calculationHistoryListSlot = slot<List<CalculationHistory>>()
-        every { view.showCalculationHistoryList(capture(calculationHistoryListSlot)) } answers { nothing }
-        every { view.showExpression(any()) } answers { nothing }
-        every { view.showResult(any()) } answers { nothing }
+        every { view.showCalculationHistoryList(capture(calculationHistoryListSlot)) }  just runs
+        every { view.showExpression(any()) }  just runs
+        every { view.showResult(any()) }  just runs
 
         presenter.addNumberToExpression(3)
         presenter.addNumberToExpression(2)
@@ -687,7 +687,7 @@ class MainPresenterTest {
 
         val expectedExpression = Expression.EMPTY + 3 + 2 + Operator.Plus + 1
         val expectedResult = 33
-        val expectedCalculationHistoryList = listOf(CalculationHistory(expectedExpression, expectedResult))
+        val expectedCalculationHistoryList = listOf(CalculationHistory(0, expectedExpression, expectedResult))
 
         assertThat(actualCalculationHistoryList).isEqualTo(expectedCalculationHistoryList)
 
@@ -697,8 +697,8 @@ class MainPresenterTest {
     @Test
     fun `계산 기록 UI 가 보여지는 상태에서 시계 버튼을 눌러 UI 를 변경하면 계산 UI 로 변경되어야 한다`() {
         // given
-        every { view.showCalculationHistoryList(any()) } answers { nothing }
-        every { view.showCalculatorUi() } answers { nothing }
+        every { view.showCalculationHistoryList(any()) }  just runs
+        every { view.showCalculatorUi() }  just runs
         presenter.toggleUiBetweenCalculatorOrHistory()
 
         // when
