0530fb2d155f44f5fa7b15da78afc0be7caab737
dla3946gns <dla3946gns@naver.com>
2022-07-31 23:26:23 +0900

    계산 기록 작업

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/RecordData.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/RecordData.kt
@@ -1,7 +1,3 @@
 package edu.nextstep.camp.calculator.domain
 
-<<<<<<< HEAD
 data class RecordData(val expression: String, val result: Int)
-=======
-data class RecordData(val expression: String, val result: Int)
->>>>>>> 2405e6f (feat: 3단계 계산기록)
