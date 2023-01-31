76ed9a0ef4926d5ee7e9db6ffe0ee5bc91b75322
galcyurio <galcyurio@gmail.com>
2023-01-31 18:41:17 +0900

    initial commit

---
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package camp.nextstep.edu.calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
