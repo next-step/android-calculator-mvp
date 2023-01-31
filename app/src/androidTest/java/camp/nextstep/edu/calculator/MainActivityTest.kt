3f9b3ea4b3595f26d265f6ad255d89c7db63e9e9
Soyeon Park <paksuua@naver.com>
2023-01-31 22:24:12 +0900

    feat: 0~9 버튼을 누르면 0~9가 보인다

---
+++ app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
@@ -0,0 +1,110 @@
+package camp.nextstep.edu.calculator
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable no-wildcard-imports
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * @author 박소연
+ * @created 2023/01/31
+ * @updated 2023/01/31
+ * @desc
+ */
+class MainActivityTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun clickButton0AndShow0() {
+        // when: '0' 버튼을 누르면
+        onView(withId(R.id.button0)).perform(click())
+
+        // then: '0'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("0")))
+    }
+
+    @Test
+    fun clickButton1AndShow1() {
+        // when: '1' 버튼을 누르면
+        onView(withId(R.id.button1)).perform(click())
+
+        // then: '1'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("1")))
+    }
+
+    @Test
+    fun clickButton2AndShow2() {
+        // when: '2' 버튼을 누르면
+        onView(withId(R.id.button2)).perform(click())
+
+        // then: '2'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("2")))
+    }
+
+    @Test
+    fun clickButton3AndShow3() {
+        // when: '3' 버튼을 누르면
+        onView(withId(R.id.button3)).perform(click())
+
+        // then: '3'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("3")))
+    }
+
+    @Test
+    fun clickButton4AndShow4() {
+        // when: '4' 버튼을 누르면
+        onView(withId(R.id.button4)).perform(click())
+
+        // then: '4'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("4")))
+    }
+
+    @Test
+    fun clickButton5AndShow5() {
+        // when: '5' 버튼을 누르면
+        onView(withId(R.id.button5)).perform(click())
+
+        // then: '5'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("5")))
+    }
+
+    @Test
+    fun clickButton6AndShow6() {
+        // when: '6' 버튼을 누르면
+        onView(withId(R.id.button6)).perform(click())
+
+        // then: '6'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("6")))
+    }
+
+    @Test
+    fun clickButton7AndShow7() {
+        // when: '7' 버튼을 누르면
+        onView(withId(R.id.button7)).perform(click())
+
+        // then: '7'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("7")))
+    }
+
+    @Test
+    fun clickButton8AndShow8() {
+        // when: '8' 버튼을 누르면
+        onView(withId(R.id.button8)).perform(click())
+
+        // then: '8'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("8")))
+    }
+
+    @Test
+    fun clickButton9AndShow9() {
+        // when: '9' 버튼을 누르면
+        onView(withId(R.id.button9)).perform(click())
+
+        // then: '9'이 보여야 한다
+        onView(withId(R.id.textView)).check(matches(withText("9")))
+    }
+}
