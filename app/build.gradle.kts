ae1292c264384f10b1bcad1d65fda0a0312c3a5c
James <bookwriterseo@gmail.com>
2023-02-02 10:21:45 +0900

    chore: 불필요한 디펜던시 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("junit:junit:4.12")
-    testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
