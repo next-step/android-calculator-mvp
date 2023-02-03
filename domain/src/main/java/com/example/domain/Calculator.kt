c08a9fa15c4b168bef9e47707aeb0175d092b7bb
JaeHwan <pon5976@naver.com>
2023-02-03 23:48:16 +0900

    domain
    - 테스트 케이스 작성

---
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -0,0 +1,75 @@
+package com.example.domain
+
+/**
+ * # 문자열 사칙 연산 계산기 구현
+ *
+ * ## 기능 요구 사항
+ * 1. 사용자가 입력한 문자열 값에 따라 사칙 연산을 수행할 수 있는 계산기를 구현해야 한다.
+ * 2. 문자열 계산기는 사칙 연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정된다. 즉, 수학에서는 곱셈, 나눗셈이 덧셈, 뺄셈 보다 먼저 계산해야 하지만 이를 무시한다.
+ * 3. 예를 들어 "2 + 3 * 4 / 2"와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력해야 한다.
+ *
+ * ## 프로그래밍 요구 사항
+ * 1. 계산기 도메인과 관련된 로직은 domain 모듈에 구현한다.
+ * 2. 메서드가 너무 많은 일을 하지 않도록 분리하기 위해 노력해 본다.
+ *
+ * ## 힌트
+ * ### 테스트할 수 있는 단위로 나누어 구현 목록을 만든다.
+ * - 덧셈
+ * - 뺄셈
+ * - 곱셈
+ * - 나눗셈
+ * - 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw
+ * - 사칙연산 기호가 아닌 경우 IllegalArgumentException throw
+ * - 사칙 연산을 모두 포함하는 기능 구현
+ * - 공백 문자열을 빈 공백 문자로 분리하려면 String 클래스의 split(" ") 메소드를 활용한다. 반복적인 패턴을 찾아 반복문으로 구현한다.
+ * */
+class Calculator {
+    /**
+     * 덧셈
+     *
+     * @param first 첫 번째 입력값
+     * @param second 두 번째 입력값
+     * */
+    fun sum(first: Int, second: Int): Int {
+        return 0
+    }
+
+    /**
+     * 뺄셈
+     *
+     * @param first 첫 번째 입력값
+     * @param second 두 번째 입력값
+     * */
+    fun sub(first: Int, second: Int): Int {
+        return 0
+    }
+
+    /**
+     * 곱셈
+     *
+     * @param first 첫 번째 입력값
+     * @param second 두 번째 입력값
+     * */
+    fun mul(first: Int, second: Int): Int {
+        return 0
+    }
+
+    /**
+     * 나눗셈
+     *
+     * @param first 첫 번째 입력값
+     * @param second 두 번째 입력값
+     * */
+    fun div(first: Int, second: Int): Int {
+        return 0
+    }
+
+    /**
+     * 사칙 연산을 모두 포함하는 기능
+     *
+     * @param operations 입력된 문자열
+     * */
+    fun evaluate(operations: String?): Int {
+        return 0
+    }
+}
---
+++ domain/src/test/java/com/example/domain/CalculatorTest.kt
@@ -0,0 +1,73 @@
+package com.example.domain
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class CalculatorTest {
+
+    @Test
+    fun `덧셈 테스트`() {
+        val calculator = Calculator()
+
+        val sum = calculator.sum(1, 1)
+        assertEquals(2, sum)
+    }
+
+    @Test
+    fun `뺄셈 테스트`() {
+        val calculator = Calculator()
+
+        val sub = calculator.sub(2, 1)
+        assertEquals(1, sub)
+    }
+
+    @Test
+    fun `곱셈 테스트`() {
+        val calculator = Calculator()
+
+        val mul = calculator.mul(3, 3)
+        assertEquals(9, mul)
+    }
+
+    @Test
+    fun `나눗셈 테스트`() {
+        val calculator = Calculator()
+
+        val div = calculator.div(4, 2)
+        assertEquals(2, div)
+    }
+
+
+    @Test
+    fun `나눗셈 0나누기 테스트`() {
+        val calculator = Calculator()
+        assertThrows(ArithmeticException::class.java) { calculator.div(4, 0) }
+    }
+
+    @Test
+    fun `널 테스트`() {
+        val calculator = Calculator()
+        assertThrows(IllegalArgumentException::class.java) { calculator.evaluate(null) }
+    }
+
+    @Test
+    fun `공백 테스트`() {
+        val calculator = Calculator()
+        assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("") }
+    }
+
+    @Test
+    fun `기호가 아닌 경우 테스트`() {
+        val calculator = Calculator()
+        assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("5 # 3") }
+    }
+
+    @Test
+    fun `사칙 연산을 모두 포함한 기능 테스트 `() {
+        val calculator = Calculator()
+
+        val evaluate: Int = calculator.evaluate("3 + 2 - 1 * 4 / 8")
+        assertEquals(2, evaluate)
+    }
+}
