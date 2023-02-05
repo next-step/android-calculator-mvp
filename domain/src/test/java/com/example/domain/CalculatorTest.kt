1eb9f463a575a82abd6a6cb6e62e52d1a38759e8
JaeHwan <pon5976@naver.com>
2023-02-06 00:04:03 +0900

    refector :
     - 패키지 구조 변경
    
     - 테스트 케이스 추가
     - Calculator Before로 변경

--- domain/src/test/java/com/example/domain/CalculatorTest.kt
+++ domain/src/test/java/com/example/domain/CalculatorTest.kt
@@ -1,97 +1,63 @@
 package com.example.domain
 
-import org.junit.Assert.*
-
+import com.nextstep.edu.calculator.domain.Calculator
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Before
 import org.junit.Test
 
 class CalculatorTest {
+    private lateinit var calculator: Calculator
 
-    @Test
-    fun `덧셈 테스트`() {
-        val calculator = Calculator()
-
-        val sum = calculator.sum(1, 1)
-        assertEquals(2, sum)
-    }
-
-    @Test
-    fun `뺄셈 테스트`() {
-        val calculator = Calculator()
-
-        val sub = calculator.sub(2, 1)
-        assertEquals(1, sub)
-    }
-
-    @Test
-    fun `곱셈 테스트`() {
-        val calculator = Calculator()
-
-        val mul = calculator.mul(3, 3)
-        assertEquals(9, mul)
-    }
-
-    @Test
-    fun `나눗셈 테스트`() {
-        val calculator = Calculator()
-
-        val div = calculator.div(4, 2)
-        assertEquals(2, div)
-    }
-
-
-    @Test
-    fun `나눗셈 0나누기 테스트`() {
-        val calculator = Calculator()
-        assertThrows(ArithmeticException::class.java) { calculator.div(4, 0) }
+    @Before
+    fun setUp() {
+        calculator = Calculator()
     }
 
     @Test
     fun `널 테스트`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate(null) }
     }
 
     @Test
     fun `공백 테스트`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("") }
     }
 
     @Test
     fun `기호가 아닌 경우 테스트`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("5 # 3") }
     }
 
     @Test
     fun `사칙 연산을 모두 포함한 기능 테스트`() {
-        val calculator = Calculator()
-
         val evaluate: Int = calculator.evaluate("3 + 2 - 1 * 4 / 8")
         assertEquals(2, evaluate)
     }
 
     @Test
+    fun `1더하기 2더하기 3`() {
+        val evaluate: Int = calculator.evaluate("1 + 2 + 3")
+        assertEquals(6, evaluate)
+    }
+
+    @Test
     fun `아무값이나 입력한 경우`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("#%@#2 154345#$ $") }
     }
 
     @Test
     fun `음수값을 입력한 경우`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("-1 + -3") }
     }
 
     @Test
     fun `입력이 다 되지 않은 상태에서 계산한 경우`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("5 +") }
     }
 
     @Test
     fun `숫자만 입력한 경우`() {
-        val calculator = Calculator()
         assertThrows(IllegalArgumentException::class.java) { calculator.evaluate("5 5 5 5 5 5 5 5 5 5") }
     }
 }
