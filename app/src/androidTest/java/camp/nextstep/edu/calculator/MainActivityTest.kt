36653ee16719d03e543ab7c90e0fad339318ba9b
JaeHwan <pon5976@naver.com>
2023-02-06 23:21:01 +0900

    feat: 모든 기능 요구 사항에 대한 UI 테스트를 구현한다

--- app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
@@ -1,14 +1,12 @@
 package camp.nextstep.edu.calculator
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.hamcrest.CoreMatchers.allOf
+import com.nextstep.edu.calculator.domain.Calculator
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,41 +16,105 @@ class MainActivityTest {
 
     @Test
     fun `입력된_피연산자가_없을_때_사용자가_피연산자_0_부터_9_버튼을_누르면_화면에_해당_숫자가_화면에_보여야_한다`() {
+        // when
+        onView(withId(R.id.button0)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.button3)).perform(click())
+        onView(withId(R.id.button4)).perform(click())
+        onView(withId(R.id.button5)).perform(click())
+        onView(withId(R.id.button6)).perform(click())
+        onView(withId(R.id.button7)).perform(click())
+        onView(withId(R.id.button8)).perform(click())
+        onView(withId(R.id.button9)).perform(click())
 
+        // then
+        onView(withId(R.id.textView)).check(matches(withText("0123456789")))
     }
 
     @Test
     fun `입력된_피연산자가_있을_때_기존_숫자_뒤에_해당_숫자가_화면에_보여야_한다_예를_들면_8이_입력되어_있을_때_9를_입력하면_89가_보여야_한다`() {
+        // when
+        onView(withId(R.id.button8)).perform(click())
+        onView(withId(R.id.button9)).perform(click())
 
+        // then
+        onView(withId(R.id.textView)).check(matches(withText("89")))
     }
 
     @Test
     fun `입력된_피연산자가_없을_때_사용자가_연산자_덧셈_뺄셈_곱셈_나눗셈_버튼을_누르면_화면에_아무런_변화가_없어야_한다`() {
+        // when  -> + 클릭 ->
+        onView(withId(R.id.buttonPlus)).perform(click())
 
+        // then
+        onView(withId(R.id.textView)).check(matches(withText("")))
     }
 
     @Test
     fun `입력된_피연산자가_있을_때_사용자가_연산자_덧셈_뺄셈_곱셈_나눗셈_버튼을_누르면_해당_기호가_화면에_보여야_한다`() {
+        // when
+        // 1 -> + 클릭 -> 1 +
+        onView(withId(R.id.button1)).perform(click())
+        onView(withId(R.id.buttonPlus)).perform(click())
+
+        onView(withId(R.id.textView)).check(matches(withText("1 +")))
+
+        // 1 + -> - 클릭 -> 1 -
+        onView(withId(R.id.buttonMinus)).perform(click())
+
+        // then
+        onView(withId(R.id.textView)).check(matches(withText("1 -")))
 
     }
 
     @Test
     fun `입력된_수식이_없을_때_사용자가_지우기_버튼을_누르면_화면에_아무런_변화가_없어야_한다`() {
+        // when
+        onView(withId(R.id.buttonDelete)).perform(click())
 
+        // then
+        onView(withId(R.id.textView)).check(matches(withText("")))
     }
 
     @Test
     fun `입력된_수식이_있을_때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_연산자_또는_피연산자가_지워져야_한다`() {
+        // when
+        // 32 + 1 -> 지우기 클릭 -> 32 + -> 지우기 클릭 -> 32 -> 지우기 클릭 -> 3 -> 지우기 클릭 ->  -> 지우기 클릭 ->
+        onView(withId(R.id.button3)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.buttonPlus)).perform(click())
+        onView(withId(R.id.button1)).perform(click())
 
+        // then
+        onView(withId(R.id.buttonDelete)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("32 +")))
+
+        onView(withId(R.id.buttonDelete)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("32")))
+
+        onView(withId(R.id.buttonDelete)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("3")))
+
+        onView(withId(R.id.buttonDelete)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("")))
+
+        onView(withId(R.id.buttonDelete)).perform(click())
+        onView(withId(R.id.textView)).check(matches(withText("")))
     }
 
     @Test
     fun `입력된_수신이_완전할_때_사용자가_등호_버튼을_누르면_입력된_수식의_결과가_화면에_보여야_한다`() {
+        // when
+        // 3 + 2 -> = 클릭 -> 5
+        onView(withId(R.id.button3)).perform(click())
+        onView(withId(R.id.buttonPlus)).perform(click())
+        onView(withId(R.id.button2)).perform(click())
+        onView(withId(R.id.buttonEquals)).perform(click())
 
-    }
 
-    @Test
-    fun `입력된_수식이_완전하지_않을_때_사용자가_등호_버튼을_눌렀을_때_완성되지_않은_수식입니다_토스트_메세지가_화면에_보여야_한다`() {
-
+        //then
+        val result = Calculator().evaluate("3 + 2")
+        onView(withId(R.id.textView)).check(matches(withText(result)))
     }
 }
