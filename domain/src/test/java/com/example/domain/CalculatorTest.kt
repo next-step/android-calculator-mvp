f640a45e2d5413f62ad3747412f28d95c46aa5dd
gyurim <gyurim1250@gmail.com>
2023-02-07 08:48:48 +0900

    chore: junit4 테스트 라이브러리 추가

---
+++ domain/src/test/java/com/example/domain/CalculatorTest.kt
@@ -0,0 +1,11 @@
+package com.example.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CalculatorTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
