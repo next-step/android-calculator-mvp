15f96fe87307084621532f92b0dce87e939b9cc8
gyurim <gyurim1250@gmail.com>
2023-02-07 08:49:33 +0900

    feat: 계산기 구현

---
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -0,0 +1,28 @@
+package com.example.domain
+
+class Calculator {
+    fun evaluate(expressions: String): Int {
+        require(!expressions.isNullOrEmpty()) { "입력값이 존재하지 않습니다." }
+
+        val expressionList = expressions.split(" ")
+        require(expressionList.size % 2 == 1) {"불완전한 수식입니다."}
+
+        var result = expressionList[0].toInt()
+
+        for (i in 1 until expressionList.size step 2) {
+            val operand = expressionList[i + 1].toIntOrNull()
+            require(operand != null) {"피연산자는 숫자만 입력할 수 있습니다."}
+
+            val op: Operator = when (expressionList[i]) {
+                Operator.PLUS.op -> Operator.PLUS
+                Operator.MINUS.op -> Operator.MINUS
+                Operator.MULTIPLY.op -> Operator.MULTIPLY
+                Operator.DIV.op -> Operator.DIV
+                else -> throw IllegalArgumentException("지원하지 않는 사칙연산 기호입니다.")
+            }
+
+            result = op.operate(result, operand)
+        }
+        return result
+    }
+}
---
+++ domain/src/main/java/com/example/domain/Operator.kt
@@ -0,0 +1,18 @@
+package com.example.domain
+
+enum class Operator(val op: String) {
+    PLUS("+") {
+        override fun operate(firstOperand: Int, secondOperand: Int): Int = firstOperand + secondOperand
+    },
+    MINUS("-") {
+        override fun operate(firstOperand: Int, secondOperand: Int): Int = firstOperand - secondOperand
+    },
+    MULTIPLY("*") {
+        override fun operate(firstOperand: Int, secondOperand: Int): Int = firstOperand * secondOperand
+    },
+    DIV("/") {
+        override fun operate(firstOperand: Int, secondOperand: Int): Int = firstOperand / secondOperand
+    };
+
+    abstract fun operate(firstOperand: Int, secondOperand: Int): Int
+}
