7e122cff487dc5fb28967193fc6edc5320dc137a
jpoh281 <hjp4869@naver.com>
2023-02-07 21:51:24 +0900

    feat: remove 기능 구현 완료

--- domain/src/main/java/com/example/domain/Statement.kt
+++ domain/src/main/java/com/example/domain/Statement.kt
@@ -3,26 +3,17 @@ package com.example.domain
 data class Statement(private var terms: List<OperationTerm> = emptyList()) {
 
     fun addTerm(term: OperationTerm) {
-        when {
-            terms.isEmpty() -> {
-                addTermWhenEmpty(term)
-            }
-            terms.last() is Operator -> {
+        when (terms.lastOrNull()) {
+            is Operator -> {
                 addTermWhenLastIsOperator(term)
             }
-            else -> {
+            is Operand -> {
                 addTermWhenLastIsOperand(term)
             }
+            null -> {
+                addTermWhenEmpty(term)
+            }
         }
-    }
-
-    fun removeTerm() {
-
-    }
-
-    fun calculate(): Int {
-        val calculator = Calculator()
-        return calculator.calculate(termsToString())
     }
 
     private fun addTermWhenEmpty(term: OperationTerm) {
@@ -49,8 +40,35 @@ data class Statement(private var terms: List<OperationTerm> = emptyList()) {
             val last = terms.last()
             terms = terms - last
         }
-
         terms = terms + term
+    }
+
+
+    fun removeTerm() {
+        when (terms.lastOrNull()) {
+            is Operator -> {
+                terms = terms - terms.last()
+            }
+            is Operand -> {
+                removeTermWhenLastIsOperand()
+            }
+        }
+    }
+
+    private fun removeTermWhenLastIsOperand() {
+        if ((terms.last() as Operand).value >= 10) {
+            val last = terms.last() as Operand
+            terms = terms - terms.last()
+            val remainder = (last.value - last.value % 10) / 10
+            terms = terms + Operand(remainder)
+        } else {
+            terms = terms - terms.last()
+        }
+    }
+
+    fun calculate(): Int {
+        val calculator = Calculator()
+        return calculator.calculate(termsToString())
     }
 
     fun termsToString(): String {
