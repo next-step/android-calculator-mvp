f48fc0bb101b7485537a35229fd58cabd1b4866d
Soyeon Park <paksuua@naver.com>
2023-02-08 01:46:31 +0900

    update: 성공 케이스 runCatching문 추가

--- domain/src/test/java/CalculatorTest.kt
+++ domain/src/test/java/CalculatorTest.kt
@@ -13,8 +13,12 @@ class CalculatorTest {
     @Test // 전체 수식 계산
     fun ` ' 2 + 3 * 4 % 2 ' 을 계산하고 10을 출력해야 한다`() {
         val calculator = Calculator()
-        val actual: Int = calculator.calculate("2+3*4/2")
-        assertThat(actual).isEqualTo(10)
+
+        runCatching {
+            calculator.calculate("2+3*4/2")
+        }.onSuccess { actual ->
+            assertThat(actual).isEqualTo(10)
+        }
     }
 
     @Test(expected = IllegalArgumentException::class)
