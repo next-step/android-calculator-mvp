019fe49f829824c662ad835bc9babccd4e6c5ae5
Soyeon Park <paksuua@naver.com>
2023-02-08 13:05:58 +0900

    chore: 함수명 및 변수명 변경 (formula -> expression)

--- domain/src/main/java/com/nextstep/calculator/Calculator.kt
+++ domain/src/main/java/com/nextstep/calculator/Calculator.kt
@@ -13,49 +13,49 @@ class Calculator {
     private val validNumber = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
     private val validOperator = charArrayOf(PLUS.char, MINUS.char, MULTIPLY.char, DIVIDE.char)
 
-    fun calculate(formula: String): Int {
+    fun calculate(expression: String): Int {
         // 입력값이 null이거나 빈 공백 문자일 경우
-        require(formula.isNotEmpty()) {
+        require(expression.isNotEmpty()) {
             IllegalArgumentException("입력값이 null이거나 빈 공백 문자")
         }
 
-        return checkFormula(formula)
+        return checkExpression(expression)
     }
 
     /**
      * 수식 내 문자 체크 후 수식 생성
      * */
-    private fun checkFormula(formula: String): Int {
+    private fun checkExpression(expression: String): Int {
         var currentOperator: Char? = null
-        var subFormula = ""
+        var subExpression = ""
 
-        for (c in formula) {
+        for (c in expression) {
             checkCharacter(
                 c,
                 currentOperator,
-                subFormula,
-                { checkFormula -> subFormula = checkFormula },
+                subExpression,
+                { checkExpression -> subExpression = checkExpression },
                 { checkCharacter -> currentOperator = checkCharacter }
             )
         }
-        return divideComponent(subFormula)
+        return divideComponent(subExpression)
     }
 
     // 수식 내 문자가 숫자인지 사친연산자인지 체크
     private fun checkCharacter(
         char: Char,
         currentOperator: Char?,
-        subFormula: String,
-        checkedFormula: (String) -> Unit,
+        subExpression: String,
+        checkedExpression: (String) -> Unit,
         checkedChar: (Char) -> Unit
     ) {
         when (char) {
-            in validNumber -> checkedFormula.invoke(subFormula + char)
+            in validNumber -> checkedExpression.invoke(subExpression + char)
             in validOperator -> {
-                checkFirstOperator(currentOperator, subFormula) {
-                    checkedFormula.invoke(it)
+                checkFirstOperator(currentOperator, subExpression) {
+                    checkedExpression.invoke(it)
                 }
-                checkedFormula.invoke(subFormula + char)
+                checkedExpression.invoke(subExpression + char)
                 checkedChar.invoke(char)
             }
             else -> throw IllegalArgumentException("사칙연산자가 아닌 기호")
@@ -65,29 +65,29 @@ class Calculator {
     // 첫번째로 나온 사칙연산자가 아니면 수식 구성요소 분리 요청
     private fun checkFirstOperator(
         currentOperator: Char?,
-        subFormula: String,
-        checkedFormula: (String) -> Unit
+        subExpression: String,
+        checkedExpression: (String) -> Unit
     ) {
         if (currentOperator != null) {
-            val calculateSubResult = divideComponent(subFormula).toString()
-            checkedFormula.invoke(calculateSubResult)
+            val calculateSubResult = divideComponent(subExpression).toString()
+            checkedExpression.invoke(calculateSubResult)
         }
     }
 
     // 수식 내 구성요소 분리
-    private fun divideComponent(formula: String): Int {
+    private fun divideComponent(expression: String): Int {
         // 수식 내 연산자 위치
-        val operatorIndex = formula.indexOfAny(validOperator, ignoreCase = false)
+        val operatorIndex = expression.indexOfAny(validOperator, ignoreCase = false)
 
-        val operator = formula[operatorIndex]
-        val num1 = formula.substring(0, operatorIndex).toInt()
-        val num2 = formula.substring(operatorIndex + 1).toInt()
+        val operator = expression[operatorIndex]
+        val num1 = expression.substring(0, operatorIndex).toInt()
+        val num2 = expression.substring(operatorIndex + 1).toInt()
 
-        return calculateSubFormula(num1, num2, operator)
+        return calculateSubExpression(num1, num2, operator)
     }
 
     // 연산자 체크 및 계산
-    private fun calculateSubFormula(num1: Int, num2: Int, operator: Char): Int {
+    private fun calculateSubExpression(num1: Int, num2: Int, operator: Char): Int {
         return when (operator) {
             PLUS.char -> plus(num1, num2)
             MINUS.char -> minus(num1, num2)
