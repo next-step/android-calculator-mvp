542f8e574da30ed3790dd177c735c80c2e9616f4
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-08 22:34:40 +0900

    refactor : 반복되는 expression 노출 & 화면에 값 보여주는 코드를 백킹 필드로 코드 정리

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -12,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private var expression = Expression()
+        set(value) {
+            field = value
+            binding.textView.text = expression.value()
+        }
+
     private val calculator = Calculator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,58 +24,52 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = appendOperandInExpression(0) }
-        binding.button1.setOnClickListener { binding.textView.text = appendOperandInExpression(1) }
-        binding.button2.setOnClickListener { binding.textView.text = appendOperandInExpression(2) }
-        binding.button3.setOnClickListener { binding.textView.text = appendOperandInExpression(3) }
-        binding.button4.setOnClickListener { binding.textView.text = appendOperandInExpression(4) }
-        binding.button5.setOnClickListener { binding.textView.text = appendOperandInExpression(5) }
-        binding.button6.setOnClickListener { binding.textView.text = appendOperandInExpression(6) }
-        binding.button7.setOnClickListener { binding.textView.text = appendOperandInExpression(7) }
-        binding.button8.setOnClickListener { binding.textView.text = appendOperandInExpression(8) }
-        binding.button9.setOnClickListener { binding.textView.text = appendOperandInExpression(9) }
 
-        binding.buttonPlus.setOnClickListener {
-            binding.textView.text = appendOperatorInExpression(Operator.PLUS)
-        }
-        binding.buttonMinus.setOnClickListener {
-            binding.textView.text = appendOperatorInExpression(Operator.MINUS)
-        }
-        binding.buttonMultiply.setOnClickListener {
-            binding.textView.text = appendOperatorInExpression(Operator.MULTI)
-        }
-        binding.buttonDivide.setOnClickListener {
-            binding.textView.text = appendOperatorInExpression(Operator.DIVIDE)
-        }
-        binding.buttonDelete.setOnClickListener { binding.textView.text = removeWordInExpression() }
+        binding.button0.setOnClickListener { appendOperandInExpression(0) }
+        binding.button1.setOnClickListener { appendOperandInExpression(1) }
+        binding.button2.setOnClickListener { appendOperandInExpression(2) }
+        binding.button3.setOnClickListener { appendOperandInExpression(3) }
+        binding.button4.setOnClickListener { appendOperandInExpression(4) }
+        binding.button5.setOnClickListener { appendOperandInExpression(5) }
+        binding.button6.setOnClickListener { appendOperandInExpression(6) }
+        binding.button7.setOnClickListener { appendOperandInExpression(7) }
+        binding.button8.setOnClickListener { appendOperandInExpression(8) }
+        binding.button9.setOnClickListener { appendOperandInExpression(9) }
+
+        binding.buttonPlus.setOnClickListener { appendOperatorInExpression(Operator.PLUS) }
+        binding.buttonMinus.setOnClickListener { appendOperatorInExpression(Operator.MINUS) }
+        binding.buttonMultiply.setOnClickListener { appendOperatorInExpression(Operator.MULTI) }
+        binding.buttonDivide.setOnClickListener { appendOperatorInExpression(Operator.DIVIDE) }
+        binding.buttonDelete.setOnClickListener { removeWordInExpression() }
         binding.buttonEquals.setOnClickListener { binding.textView.text = returnResult() }
     }
 
     private fun returnResult(): String {
         return try {
             calculator.evaluate(expression.value()).toString()
-        }catch (exception: IllegalArgumentException) {
+        } catch (exception: IllegalArgumentException) {
             showToastMessage()
             expression.value()
         }
     }
 
     private fun showToastMessage() {
-        Toast.makeText(applicationContext, "완성되지 않은 수식입니다. \n${expression.value()}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            applicationContext,
+            "완성되지 않은 수식입니다. \n${expression.value()}",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
-    private fun removeWordInExpression(): String {
+    private fun removeWordInExpression() {
         expression = expression.dropLastInExpression()
-        return expression.value()
     }
 
-    private fun appendOperandInExpression(value: Int): String {
+    private fun appendOperandInExpression(value: Int) {
         expression += value
-        return expression.value()
     }
 
-    private fun appendOperatorInExpression(value: Operator): String {
+    private fun appendOperatorInExpression(value: Operator) {
         expression += value
-        return expression.value()
     }
 }
