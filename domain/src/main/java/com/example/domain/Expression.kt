90bbff8e5f3677ca3333516f4eb2687a330d6be2
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-08 14:57:48 +0900

    feat : 지우기 버튼 구현

--- domain/src/main/java/com/example/domain/Expression.kt
+++ domain/src/main/java/com/example/domain/Expression.kt
@@ -3,25 +3,13 @@ package com.example.domain
 data class Expression(
     private val values: List<Any> = emptyList()
 ) {
-
-    /**
-    값이 아무 것도 없을 경우, 값이 있을 경우
-     */
-    fun append(input: Any): Expression {
-        return when (input) {
-            is Operator -> plus(input)
-            is Int -> plus(input)
-            else -> throw IllegalArgumentException("해당 값은 수식에 필요한 값이 아닙니다.")
-        }
-    }
-
     /**
     들어올 케이스
     [3, +]   ->   1   = [3, +, 1]
     [3]      ->   1   = [31]
     [+]      ->   1   = [1]
      */
-    private operator fun plus(operand: Int): Expression {
+    operator fun plus(operand: Int): Expression {
         return when (val last = values.lastOrNull()) {
             is Operator -> {
                 if (values.size == 1) return Expression(listOf(operand))
@@ -43,7 +31,7 @@ data class Expression(
     [+]     ->   *   = [*]
     [3]     ->   *   = [3, *]
      */
-    private operator fun plus(operator: Operator): Expression {
+    operator fun plus(operator: Operator): Expression {
         return when (val last = values.lastOrNull()) {
             is Operator -> {
                 Expression(values.dropLast(1) + operator)
@@ -53,6 +41,12 @@ data class Expression(
 
             else -> throw IllegalArgumentException("연산자를 추가하는 과정에서 Error 발생")
         }
+    }
+
+    fun dropLastInExpression(): Expression {
+        if (values.isNullOrEmpty()) return Expression()
+
+        return Expression(values.dropLast(1))
     }
 
     fun value(): String {
