7c631eb4100d0db3419bcc6c0e308b30f585c86e
gyurim <gyurim1250@gmail.com>
2023-02-08 23:19:10 +0900

    chore: operator 타입 가져오는 로직 분리

--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -7,21 +7,14 @@ class Calculator {
         val expressionList = expressions.split(" ")
         require(expressionList.size % 2 == 1) { "불완전한 수식입니다." }
 
-        var result = expressionList[0].toInt()
+        var result = expressionList[0].toIntOrNull()
+        require(result != null) { "첫번째 인자는 숫자여야합니다." }
 
         for (i in 1 until expressionList.size step 2) {
             val operand = expressionList[i + 1].toIntOrNull()
             require(operand != null) { "피연산자는 숫자만 입력할 수 있습니다." }
 
-            val op: Operator = when (expressionList[i]) {
-                Operator.PLUS.op -> Operator.PLUS
-                Operator.MINUS.op -> Operator.MINUS
-                Operator.MULTIPLY.op -> Operator.MULTIPLY
-                Operator.DIV.op -> Operator.DIV
-                else -> throw IllegalArgumentException("지원하지 않는 사칙연산 기호입니다.")
-            }
-
-            result = op.operate(result, operand)
+            val operator = Operator.of(expressionList[i])
         }
         return result
     }
--- domain/src/main/java/com/example/domain/Operator.kt
+++ domain/src/main/java/com/example/domain/Operator.kt
@@ -20,4 +20,15 @@ enum class Operator(val op: String) {
     };
 
     abstract fun operate(firstOperand: Int, secondOperand: Int): Int
+
+    companion object {
+        fun of(op: String): Operator {
+            val validOperator = values().find { operator ->
+                op == operator.op
+            }
+
+            require(validOperator != null) { "지원하지 않는 사칙연산 기호입니다." }
+            return validOperator
+        }
+    }
 }
