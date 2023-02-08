1780687e54ceff8e30eb8bbeef135cd6cd826faf
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-08 15:50:59 +0900

    test : Expression 로직 수정으로 인한 수정

--- domain/src/test/java/com/example/domain/ExpressionTest.kt
+++ domain/src/test/java/com/example/domain/ExpressionTest.kt
@@ -9,11 +9,11 @@ class ExpressionTest {
     @Test
     fun `Expression {피연산자, 연산자} 에서 피연산자가 들어올 경우`() {
         // given
-        expression = expression.append(3)
-        expression = expression.append(Operator.PLUS)
+        expression = expression.plus(3)
+        expression = expression.plus(Operator.PLUS)
 
         // when
-        expression = expression.append(3)
+        expression = expression.plus(3)
 
         // then
         assertEquals("3 + 3", expression.value())
@@ -22,10 +22,10 @@ class ExpressionTest {
     @Test
     fun `Expression {피연산자} 에서 피연산자가 들어올 경우`() {
         // given
-        expression = expression.append(3)
+        expression = expression.plus(3)
 
         // when
-        expression = expression.append(3)
+        expression = expression.plus(3)
 
         // then
         assertEquals("33", expression.value())
@@ -34,9 +34,9 @@ class ExpressionTest {
     @Test
     fun `Expression {연산자} 에서 피연산자가 들어올 경우 연산자 무시되고 피연산자로 덮어 쓰인다`() {
         // given
-        expression = expression.append(Operator.PLUS)
+        expression = expression.plus(Operator.PLUS)
         // when
-        expression = expression.append(1)
+        expression = expression.plus(1)
 
         // then
         assertEquals("1", expression.value())
@@ -45,10 +45,10 @@ class ExpressionTest {
     @Test
     fun `Expression {피연산자, 연산자} 에서 연산자가 들어올 경우 마지막에 들어온 연산자로 덮어 쓰인다`() {
         // given
-        expression = expression.append(3)
-        expression = expression.append(Operator.PLUS)
+        expression = expression.plus(3)
+        expression = expression.plus(Operator.PLUS)
         // when
-        expression = expression.append(Operator.MULTI)
+        expression = expression.plus(Operator.MULTI)
 
         // then
         assertEquals("3 *", expression.value())
@@ -57,22 +57,22 @@ class ExpressionTest {
     @Test
     fun `Expression {연산자} 에서 연산자가 들어올 경우`() {
         // given
-        expression = expression.append(Operator.PLUS)
+        expression = expression.plus(Operator.PLUS)
 
         // when
-        expression = expression.append(Operator.MULTI)
+        expression = expression.plus(Operator.MULTI)
 
         // then
-        assertEquals("*", expression.value())
+        assertEquals("", expression.value())
     }
 
     @Test
     fun `Expression {피연산자} 에서 연산자가 들어올 경우`() {
         // given
-        expression = expression.append(3)
+        expression = expression.plus(3)
 
         // when
-        expression = expression.append(Operator.MULTI)
+        expression = expression.plus(Operator.MULTI)
 
         // then
         assertEquals("3 *", expression.value())
