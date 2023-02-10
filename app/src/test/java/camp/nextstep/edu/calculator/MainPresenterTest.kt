348db399d1facaa2a5dd228cb5d73a5c9f6c412f
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-02-11 08:14:19 +0900

    Update expression-result test code

--- app/src/test/java/camp/nextstep/edu/calculator/MainPresenterTest.kt
+++ app/src/test/java/camp/nextstep/edu/calculator/MainPresenterTest.kt
@@ -71,20 +71,21 @@ class MainPresenterTest {
     @Test
     fun `옳바른 수식 상태에서 이퀄사인 버튼을 누를 경우 수식을 계산한 결과 값이 보여야 한다`() {
         // given
-        val expressionSlot: CapturingSlot<Expression> = slot()
+        val calculator: Calculator = mockk()
+        val resultSlot = slot<String>()
+        val expressionSlot = slot<Expression>()
         every { view.showExpression(capture(expressionSlot)) } just Runs
-        every { view.showResult("6") } just Runs
-
+        every { calculator.evaluate(capture(resultSlot)) } returns 6
+        every { view.showResult(capture(resultSlot)) } just Runs
         presenter.appendOperandInExpression(3)
         presenter.appendOperatorInExpression(Operator.PLUS)
         presenter.appendOperandInExpression(3)
 
         // when
         presenter.returnResult()
-        val expression = expressionSlot.captured
-        val result = Calculator().evaluate(expression.value()).toString()
 
         // then
+        val result = calculator.evaluate(expressionSlot.captured.value()).toString()
         assertEquals("6", result)
         verify { view.showResult(result) }
     }
