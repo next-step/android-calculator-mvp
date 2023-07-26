1f95146e7335b37d814bed4a316e5ab460573d2d
wefu <wefu00@gmail.com>
2023-07-27 00:02:59 +0900

    style: klint 체크에 맞게 수정

--- app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
@@ -1,14 +1,11 @@
 package camp.nextstep.edu.calculator
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,62 +15,71 @@ class MainActivityTest {
 
     @Test
     fun button0() {
-        onView(withId(R.id.button1)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("0")))
+        onView(withId(R.id.button0)).perform(click())
+
+        onView(withId(R.id.textView)).check(matches(withText("0")))
     }
 
     @Test
     fun button1() {
         onView(withId(R.id.button1)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("1")))
+
+        onView(withId(R.id.textView)).check(matches(withText("1")))
     }
 
     @Test
     fun button2() {
         onView(withId(R.id.button2)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("2")))
+
+        onView(withId(R.id.textView)).check(matches(withText("2")))
     }
 
     @Test
     fun button3() {
         onView(withId(R.id.button3)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("3")))
+
+        onView(withId(R.id.textView)).check(matches(withText("3")))
     }
 
     @Test
     fun button4() {
         onView(withId(R.id.button4)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("4")))
+
+        onView(withId(R.id.textView)).check(matches(withText("4")))
     }
 
     @Test
     fun button5() {
         onView(withId(R.id.button5)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("5")))
+
+        onView(withId(R.id.textView)).check(matches(withText("5")))
     }
 
     @Test
     fun button6() {
         onView(withId(R.id.button6)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("6")))
+
+        onView(withId(R.id.textView)).check(matches(withText("6")))
     }
 
     @Test
     fun button7() {
         onView(withId(R.id.button7)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("7")))
+
+        onView(withId(R.id.textView)).check(matches(withText("7")))
     }
 
     @Test
     fun button8() {
         onView(withId(R.id.button8)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("8")))
+
+        onView(withId(R.id.textView)).check(matches(withText("8")))
     }
 
     @Test
     fun button9() {
         onView(withId(R.id.button9)).perform(click())
-        onView(withId(R.id.textView)).check(ViewAssertions.matches(withText("9")))
-    }
 
+        onView(withId(R.id.textView)).check(matches(withText("9")))
+    }
 }
