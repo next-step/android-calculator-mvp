ab59a8d8616c863d4ff32654626340df6c5e0cba
suyong-park <spdlqjfire@naver.com>
2023-07-27 00:28:23 +0900

    domain 모듈 implementation 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain"))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin}")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
@@ -54,6 +56,4 @@ dependencies {
     testImplementation("androidx.arch.core:core-testing:2.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation(":domain")
 }
