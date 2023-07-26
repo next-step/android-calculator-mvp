3b026e25e2f0f41995c5a72cb617b12995022c3a
suyong-park <spdlqjfire@naver.com>
2023-07-27 01:05:09 +0900

    domain 모듈 추가

--- app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
@@ -21,7 +21,7 @@ class MainActivityTest {
         // then : 0이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("0")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("0")).check(matches(isDisplayed()))
     }
 
     @Test
@@ -43,83 +43,83 @@ class MainActivityTest {
         // then : 2이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("2")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("2")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button3() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 3을 클릭하면
         onView(withId(R.id.button3)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 3이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("3")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("3")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button4() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 4을 클릭하면
         onView(withId(R.id.button4)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 4이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("4")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("4")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button5() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 5을 클릭하면
         onView(withId(R.id.button5)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 5이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("5")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("5")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button6() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 6을 클릭하면
         onView(withId(R.id.button6)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 6이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("6")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("6")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button7() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 7을 클릭하면
         onView(withId(R.id.button7)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 7이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("7")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("7")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button8() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 8을 클릭하면
         onView(withId(R.id.button8)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 8이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("8")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("8")).check(matches(isDisplayed()))
     }
 
     @Test
     fun button9() {
-        // when : 계산기 버튼 1을 클릭하면
+        // when : 계산기 버튼 9을 클릭하면
         onView(withId(R.id.button9)).perform(click())
 
-        // then : 1이 보여야 한다.
+        // then : 9이 보여야 한다.
         // 위 방식은 화이트 박스 테스트이며, 아래는 블랙 박스 테스트이다.
         onView(withId(R.id.textView)).check(matches(withText("9")))
-        // onView(withText("1")).check(matches(isDisplayed()))
+        // onView(withText("9")).check(matches(isDisplayed()))
     }
 }
