0150834380b6e3996fc978a6bb3fb9020000ccdf
duru.hi <hwanggyuil@gmail.com>
2023-07-29 14:04:48 +0900

    fix. require 코멘트 반영(if throw -> require 수정)

--- domain/src/main/java/com/nextstep/edu/domain/CalculatorExpression.kt
+++ domain/src/main/java/com/nextstep/edu/domain/CalculatorExpression.kt
@@ -2,17 +2,13 @@ package com.nextstep.edu.domain
 
 class CalculationExpression {
     fun split(inputValue: String?): List<String> {
-        if (inputValue.isNullOrBlank()) {
-            throw IllegalArgumentException("Input Value is Null Or Blank")
-        }
+        require(!inputValue.isNullOrBlank()) { "Input Value is Null Or Blank" }
         return inputValue.split(VALUE_DIVIDER)
     }
 
     fun validate(splitInputValue: List<String>) {
         splitInputValue.forEach {
-            if (it.isBlank()) {
-                throw IllegalArgumentException("Input Value is Blank")
-            }
+            require(it.isNotBlank()) { "Input Value is Blank" }
         }
     }
 
