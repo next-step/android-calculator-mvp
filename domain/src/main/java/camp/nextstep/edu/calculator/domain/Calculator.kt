a238e12484553d1774a3b6b9aa26f53189d29055
wefu <wefu00@gmail.com>
2023-08-01 02:24:59 +0900

    fix: evaluateValue 로컬 변수로 변경

--- domain/src/main/java/camp/nextstep/edu/calculator/domain/Calculator.kt
+++ domain/src/main/java/camp/nextstep/edu/calculator/domain/Calculator.kt
@@ -1,15 +1,16 @@
 package camp.nextstep.edu.calculator.domain
 
 class Calculator(private val convertor: InputTextConvertor) {
-    private var evaluateValue = 0
-
     fun evaluate(input: String?): Int {
         val splitArray = convertor.getSplitStrings(input)
         var operation: Operations? = null
+        var evaluateValue = 0
 
         splitArray.forEachIndexed { index, str ->
             if (index % 2 == 0) {
-                evaluate(operation, convertor.getNumberText(str))
+                evaluateValue = evaluate(
+                    operation, evaluateValue, convertor.getNumberText(str)
+                )
             } else {
                 operation = convertor.getOperationText(str)
             }
@@ -17,8 +18,8 @@ class Calculator(private val convertor: InputTextConvertor) {
         return evaluateValue
     }
 
-    private fun evaluate(operation: Operations?, value: Int) {
-        evaluateValue = operation?.run {
+    private fun evaluate(operation: Operations?, evaluateValue: Int, value: Int): Int {
+        return operation?.run {
             evaluate(evaluateValue, value)
         } ?: value
     }
