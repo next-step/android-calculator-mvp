30351a6b2f2f380d7c8bb154b2230fdc07aec1b0
duru.hi <hwanggyuil@gmail.com>
2023-07-31 20:22:52 +0900

    fix. step4 피드백 반영 (번호 클릭 리스너 공통 내용 통합 & runCatching 메서드로 수정)

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -1,15 +1,29 @@
 package camp.nextstep.edu.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
 import com.nextstep.edu.domain.Expression
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     private val expression by lazy { Expression() }
+
+    private val numberClickListener = View.OnClickListener { v ->
+        expression.setOperands((v as Button).text.toString())
+        setResultText()
+    }
+
+    private val methodClickListener = View.OnClickListener { v ->
+        expression.setMethods((v as Button).text.toString())
+        setResultText()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,99 +32,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initClickListener() {
-        binding.button0.setOnClickListener {
-            expression.setOperands("0")
-            binding.textView.text = expression.getOperands()
-        }
 
-        binding.button1.setOnClickListener {
-            expression.setOperands("1")
-            binding.textView.text = expression.getOperands()
-        }
+        binding.button0.setOnClickListener(numberClickListener)
+        binding.button1.setOnClickListener(numberClickListener)
+        binding.button2.setOnClickListener(numberClickListener)
+        binding.button3.setOnClickListener(numberClickListener)
+        binding.button4.setOnClickListener(numberClickListener)
+        binding.button5.setOnClickListener(numberClickListener)
+        binding.button6.setOnClickListener(numberClickListener)
+        binding.button7.setOnClickListener(numberClickListener)
+        binding.button8.setOnClickListener(numberClickListener)
+        binding.button9.setOnClickListener(numberClickListener)
 
-        binding.button2.setOnClickListener {
-            expression.setOperands("2")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button3.setOnClickListener {
-            expression.setOperands("3")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button4.setOnClickListener {
-            expression.setOperands("4")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button5.setOnClickListener {
-            expression.setOperands("5")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button6.setOnClickListener {
-            expression.setOperands("6")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button7.setOnClickListener {
-            expression.setOperands("7")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button8.setOnClickListener {
-            expression.setOperands("8")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button9.setOnClickListener {
-            expression.setOperands("9")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.button0.setOnClickListener {
-            expression.setOperands("0")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.buttonPlus.setOnClickListener {
-            expression.setMethods("+")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.buttonMinus.setOnClickListener {
-            expression.setMethods("-")
-            binding.textView.text = expression.getOperands()
-        }
+        binding.buttonPlus.setOnClickListener(methodClickListener)
+        binding.buttonMinus.setOnClickListener(methodClickListener)
+        binding.buttonDivide.setOnClickListener(methodClickListener)
 
         binding.buttonMultiply.setOnClickListener {
             expression.setMethods("*")
-            binding.textView.text = expression.getOperands()
-        }
-
-        binding.buttonDivide.setOnClickListener {
-            expression.setMethods("÷")
-            binding.textView.text = expression.getOperands()
+            setResultText()
         }
 
         binding.buttonDelete.setOnClickListener {
-            expression.removeOperands()
+            expression.removeLatestOperands()
             binding.textView.text = expression.getOperands()
         }
 
         binding.buttonEquals.setOnClickListener {
-            runCatching {
-                expression.resultCalculate()
-            }.onSuccess {
-                binding.textView.text = expression.getOperands()
-            }.onFailure {
-                Toast.makeText(this@MainActivity, getString(R.string.incomplete_expression), Toast.LENGTH_SHORT).show()
-            }
+            onResultEquals()
         }
 
         binding.buttonMemory.setOnClickListener {
             expression.resetMemory()
             binding.textView.text = expression.getOperands()
         }
+    }
+
+    private fun onResultEquals() {
+        runCatching {
+            expression.resultCalculate()
+        }.onSuccess {
+            setResultText()
+        }.onFailure {
+            Toast.makeText(this@MainActivity, getString(R.string.incomplete_expression), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setResultText() {
+        binding.textView.text = expression.getOperands()
     }
 }
