ea302c841d20dc6128666fb6543205c4fb90efd8
OYJ <oyj7677@gmail.lcom>
2023-07-31 23:10:35 +0900

    step3 개선 사항 - ParameterizedTest Class 분리 - 코드 가독성 개선 	- kotlin.runCatching{ } → runCatching{ } 	- String.require() 사용 및 isEmpty확장함수 사용 - enum 활용 개선 	- Operator객체 반환하여 사용

--- calculatorLib/src/main/java/com/example/calculatorlib/Operator.kt
+++ calculatorLib/src/main/java/com/example/calculatorlib/Operator.kt
@@ -4,5 +4,11 @@ enum class Operator(val operator: String) {
     PLUS("+"),
     MINUS("-"),
     MULTIPLIED("*"),
-    DIVIDED("/")
+    DIVIDED("/");
+
+    companion object {
+        fun find(operator: String): Operator {
+            return values().find { it.operator == operator } ?: throw IllegalArgumentException("사칙연산 기호가 아닌 경우 IllegalArgumentException 발행")
+        }
+    }
 }
