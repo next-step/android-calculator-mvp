7e84ac3bba4190c1f3779d9f3ebaeb17b83c7343
김영욱 <0.uk.kim@navercorp.com>
2023-08-01 20:38:23 +0900

    연산자, 피연산자 클릭 리스너 추가

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -3,24 +3,59 @@ package camp.nextstep.edu.calculator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
+import camp.nextstep.edu.domain.Expression
+import camp.nextstep.edu.domain.Operator
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+	private lateinit var binding: ActivityMainBinding
+	private lateinit var expression: Expression
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
-    }
+		initExpression()
+
+		setOperandListener()
+		setOperatorListener()
+	}
+
+	private fun initExpression() {
+		expression = Expression()
+	}
+
+	private fun setOperandListener() {
+		binding.button0.setOnClickListener { insertOperand("0") }
+		binding.button1.setOnClickListener { insertOperand("1") }
+		binding.button2.setOnClickListener { insertOperand("2") }
+		binding.button3.setOnClickListener { insertOperand("3") }
+		binding.button4.setOnClickListener { insertOperand("4") }
+		binding.button5.setOnClickListener { insertOperand("5") }
+		binding.button6.setOnClickListener { insertOperand("6") }
+		binding.button7.setOnClickListener { insertOperand("7") }
+		binding.button8.setOnClickListener { insertOperand("8") }
+		binding.button9.setOnClickListener { insertOperand("9") }
+	}
+
+	private fun insertOperand(operand: String) {
+		expression.insertOperand(operand)
+		showExpression()
+	}
+
+	private fun setOperatorListener() {
+		binding.buttonPlus.setOnClickListener { insertOperator(Operator.PLUS) }
+		binding.buttonMinus.setOnClickListener { insertOperator(Operator.MINUS) }
+		binding.buttonMultiply.setOnClickListener { insertOperator(Operator.MUL) }
+		binding.buttonDivide.setOnClickListener { insertOperator(Operator.DIV) }
+	}
+
+	private fun insertOperator(operator: Operator) {
+		expression.insertOperator(operator)
+		showExpression()
+	}
+
+	private fun showExpression() {
+		binding.textView.text = expression.toString()
+	}
 }
