15df6b9612780952497e0dd11e0e69e52b841490
sezikim <kdghj1002@gmail.com>
2023-08-02 00:16:50 +0900

    feat: MVC 문자열 계산기

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -1,26 +1,92 @@
 package camp.nextstep.edu.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
+import camp.nextstep.edu.calculator.domain.ArithmeticExpression
+import camp.nextstep.edu.calculator.domain.ArithmeticOperator
+import camp.nextstep.edu.calculator.domain.Calculator
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var calculator: Calculator
+    private var currentInput: String = ""
+        set(value) {
+            field = value
+            binding.textView.text = currentInput
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        calculator = Calculator()
+
+        with(binding) {
+            button0.setOnClickListener { currentInput += "0" }
+            button1.setOnClickListener { currentInput += "1" }
+            button2.setOnClickListener { currentInput += "2" }
+            button3.setOnClickListener { currentInput += "3" }
+            button4.setOnClickListener { currentInput += "4" }
+            button5.setOnClickListener { currentInput += "5" }
+            button6.setOnClickListener { currentInput += "6" }
+            button7.setOnClickListener { currentInput += "7" }
+            button8.setOnClickListener { currentInput += "8" }
+            button9.setOnClickListener { currentInput += "9" }
+
+            buttonPlus.setOnClickListener {
+                setOperatorClickListener(ArithmeticOperator.PLUS)
+            }
+            buttonMinus.setOnClickListener {
+                setOperatorClickListener(ArithmeticOperator.MINUS)
+            }
+            buttonMultiply.setOnClickListener {
+                setOperatorClickListener(ArithmeticOperator.MULTIPLY)
+            }
+            buttonDivide.setOnClickListener {
+                setOperatorClickListener(ArithmeticOperator.DIVIDE)
+            }
+            buttonEquals.setOnClickListener {
+                setEqualsClickListener()
+            }
+            buttonDelete.setOnClickListener {
+                setDeleteClickListener()
+            }
+        }
+    }
+
+    private fun setOperatorClickListener(operator: ArithmeticOperator) {
+        if (currentInput.last().isDigit()) {
+            currentInput += " ${operator.value} "
+            return
+        }
+
+        if (ArithmeticOperator.isArithmeticOperator(currentInput.trimEnd().last().toString())) {
+            currentInput =
+                currentInput.dropLast(3) + " ${operator.value} "
+        }
+    }
+
+    private fun setEqualsClickListener() {
+        kotlin.runCatching {
+            calculator.calculate(ArithmeticExpression(currentInput)).toString()
+        }.onSuccess { result ->
+            currentInput = result
+        }.onFailure { exception ->
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setDeleteClickListener() {
+        if (currentInput.last().isDigit()) {
+            currentInput = currentInput.dropLast(1)
+            return
+        }
+
+        if (ArithmeticOperator.isArithmeticOperator(currentInput.trimEnd().last().toString())) {
+            currentInput = currentInput.dropLast(3)
+        }
     }
 }
