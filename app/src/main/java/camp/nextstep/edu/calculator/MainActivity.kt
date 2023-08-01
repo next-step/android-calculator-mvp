84cd77648d9488b736d7d26f80505d0cdf9e3670
duru.hi <hwanggyuil@gmail.com>
2023-08-01 14:56:06 +0900

    fix. step4 피드백 반영(View.OnClickListener 내부 Button에 종속적이던 문제 수정)

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -2,27 +2,40 @@ package camp.nextstep.edu.calculator
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
+import com.nextstep.edu.domain.Calculator
 import com.nextstep.edu.domain.Expression
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val expression by lazy { Expression() }
+    private val expression = Expression(calculator = Calculator)
 
-    private val numberClickListener = View.OnClickListener { v ->
-        expression.setOperands((v as Button).text.toString())
+    private val onClickListener = View.OnClickListener { view ->
+        when (view) {
+            binding.button0 -> expression.setOperands("0")
+            binding.button1 -> expression.setOperands("1")
+            binding.button2 -> expression.setOperands("2")
+            binding.button3 -> expression.setOperands("3")
+            binding.button4 -> expression.setOperands("4")
+            binding.button5 -> expression.setOperands("5")
+            binding.button6 -> expression.setOperands("6")
+            binding.button7 -> expression.setOperands("7")
+            binding.button8 -> expression.setOperands("8")
+            binding.button9 -> expression.setOperands("9")
+            binding.buttonPlus -> expression.setMethods("+")
+            binding.buttonMinus -> expression.setMethods("-")
+            binding.buttonMultiply -> expression.setMethods("*")
+            binding.buttonDivide -> expression.setMethods("÷")
+            binding.buttonDelete -> expression.removeLatestExpression()
+            binding.buttonMemory -> expression.resetMemory()
+        }
         setResultText()
     }
 
-    private val methodClickListener = View.OnClickListener { v ->
-        expression.setMethods((v as Button).text.toString())
-        setResultText()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,38 +46,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun initClickListener() {
 
-        binding.button0.setOnClickListener(numberClickListener)
-        binding.button1.setOnClickListener(numberClickListener)
-        binding.button2.setOnClickListener(numberClickListener)
-        binding.button3.setOnClickListener(numberClickListener)
-        binding.button4.setOnClickListener(numberClickListener)
-        binding.button5.setOnClickListener(numberClickListener)
-        binding.button6.setOnClickListener(numberClickListener)
-        binding.button7.setOnClickListener(numberClickListener)
-        binding.button8.setOnClickListener(numberClickListener)
-        binding.button9.setOnClickListener(numberClickListener)
+        binding.button0.setOnClickListener(onClickListener)
+        binding.button1.setOnClickListener(onClickListener)
+        binding.button2.setOnClickListener(onClickListener)
+        binding.button3.setOnClickListener(onClickListener)
+        binding.button4.setOnClickListener(onClickListener)
+        binding.button5.setOnClickListener(onClickListener)
+        binding.button6.setOnClickListener(onClickListener)
+        binding.button7.setOnClickListener(onClickListener)
+        binding.button8.setOnClickListener(onClickListener)
+        binding.button9.setOnClickListener(onClickListener)
 
-        binding.buttonPlus.setOnClickListener(methodClickListener)
-        binding.buttonMinus.setOnClickListener(methodClickListener)
-        binding.buttonDivide.setOnClickListener(methodClickListener)
+        binding.buttonPlus.setOnClickListener(onClickListener)
+        binding.buttonMinus.setOnClickListener(onClickListener)
+        binding.buttonDivide.setOnClickListener(onClickListener)
+        binding.buttonMultiply.setOnClickListener(onClickListener)
 
-        binding.buttonMultiply.setOnClickListener {
-            expression.setMethods("*")
-            setResultText()
-        }
-
-        binding.buttonDelete.setOnClickListener {
-            expression.removeLatestOperands()
-            binding.textView.text = expression.getOperands()
-        }
+        binding.buttonDelete.setOnClickListener(onClickListener)
+        binding.buttonMemory.setOnClickListener(onClickListener)
 
         binding.buttonEquals.setOnClickListener {
             onResultEquals()
-        }
-
-        binding.buttonMemory.setOnClickListener {
-            expression.resetMemory()
-            binding.textView.text = expression.getOperands()
         }
     }
 
@@ -79,6 +81,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setResultText() {
-        binding.textView.text = expression.getOperands()
+        binding.textView.text = expression.expressions
     }
 }
