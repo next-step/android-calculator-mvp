2548aa3fa2ae68f3e84880e2a8a119de354e6b63
KwonDae <green201402317@gmail.com>
2023-08-03 22:22:17 +0900

    중복코드 함수 분리

--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -6,13 +6,9 @@ object Calculator {
 
     fun calculate(input: String): Operand {
         require(input.isNotBlank()) { "빈 공백은 입력할 수 없습니다." }
-        val inputOperands = splitList(input).filterIndexed { index, _ ->
-            index % 2 == 0
-        }
+        val inputOperands = input.getListByDivRest(0)
 
-        val inputOperators = splitList(input).filterIndexed { index, _ ->
-            index % 2 == 1
-        }
+        val inputOperators = input.getListByDivRest(1)
         val operands = toOperandsList(inputOperands)
         val operators = toOperatorList(inputOperators)
 
@@ -22,6 +18,10 @@ object Calculator {
     }
 
     private fun splitList(input: String): List<String> = input.split(" ")
+
+    private fun String.getListByDivRest(rest: Int): List<String> = splitList(this).filterIndexed { index, _ ->
+        index % 2 == rest
+    }
 
     private fun toOperandsList(inputOperands: List<String>): List<Operand> = inputOperands.map {
         Operand(it.toBigDecimal())
