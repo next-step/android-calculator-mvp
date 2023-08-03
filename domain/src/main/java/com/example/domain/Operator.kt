8f1ad529a7553f82d8d40903041a1d0970fd64e5
KwonDae <green201402317@gmail.com>
2023-08-03 22:42:31 +0900

    Divide 나누기 0 로직 에러 명시

--- domain/src/main/java/com/example/domain/Operator.kt
+++ domain/src/main/java/com/example/domain/Operator.kt
@@ -19,7 +19,9 @@ enum class Operator(
     DIV("/", { first, second ->
         runCatching {
             first.div(second)
-        }.getOrDefault(Operand(0))
+        }.getOrElse {
+            throw IllegalArgumentException("0으로 나눌 수 없습니다.")
+        }
     });
 }
 
