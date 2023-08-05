6dc8fae72e73051f5e364448f12a8b0677ccc4ee
sezikim <kdghj1002@gmail.com>
2023-08-05 20:02:28 +0900

    refactor: step4 리뷰반영

--- domain/src/main/java/camp/nextstep/edu/calculator/domain/Expression.kt
+++ domain/src/main/java/camp/nextstep/edu/calculator/domain/Expression.kt
@@ -1,48 +1,50 @@
 package camp.nextstep.edu.calculator.domain
 
-class Expression(
-    private val onValueChanged: (String) -> Unit,
-) {
-    var value: String = ""
+class Expression(value: String) {
+    var value: String = value
         private set
 
     fun setOperand(operand: String) {
         value += operand
-        onValueChanged(value)
     }
 
     fun setOperator(operator: String) {
         if (value.isEmpty()) return
 
-        if (value.last().isDigit()) {
+        if (isLastStringOperand()) {
             value += " $operator "
-            onValueChanged(value)
             return
         }
 
-        if (ArithmeticOperator.isArithmeticOperator(value.trimEnd().last().toString())) {
-            value = value.dropLast(3) + " $operator "
-            onValueChanged(value)
+        if (isLastStringOperator()) {
+            value = value.dropLast(OPERATOR_CONCAT_STRING_LENGTH) + " $operator "
         }
     }
 
     fun setEquals(result: String) {
         value = result
-        onValueChanged(value)
     }
 
     fun setDelete() {
         if (value.isEmpty()) return
 
-        if (value.last().isDigit()) {
-            value = value.dropLast(1)
-            onValueChanged(value)
+        if (isLastStringOperand()) {
+            value = value.dropLast(OPERAND_CONCAT_STRING_LENGTH)
             return
         }
 
-        if (ArithmeticOperator.isArithmeticOperator(value.trimEnd().last().toString())) {
-            value = value.dropLast(3)
-            onValueChanged(value)
+        if (isLastStringOperator()) {
+            value = value.dropLast(OPERATOR_CONCAT_STRING_LENGTH)
         }
+    }
+
+    private fun isLastStringOperator() =
+        ArithmeticOperator.isArithmeticOperator(value.trimEnd().last().toString())
+
+    private fun isLastStringOperand() = value.last().isDigit()
+
+    companion object {
+        private const val OPERAND_CONCAT_STRING_LENGTH = 1
+        private const val OPERATOR_CONCAT_STRING_LENGTH = 3
     }
 }
