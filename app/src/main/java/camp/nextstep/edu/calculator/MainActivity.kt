62131550fb59be57afdde7a930c5c8f56b45288f
sezikim <kdghj1002@gmail.com>
2023-08-05 20:03:20 +0900

    feat: mvp 계산기 구현

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -4,72 +4,56 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
-import camp.nextstep.edu.calculator.domain.ArithmeticExpression
 import camp.nextstep.edu.calculator.domain.ArithmeticOperator
-import camp.nextstep.edu.calculator.domain.Calculator
-import camp.nextstep.edu.calculator.domain.Expression
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var calculator: Calculator
-    private lateinit var expression: Expression
+    override lateinit var presenter: MainContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        calculator = Calculator()
-        expression = Expression { value -> binding.textView.text = value }
-
+        presenter = MainPresenterImpl(this)
         initOperandButtons()
         initOperatorButtons()
 
         with(binding) {
-            buttonEquals.setOnClickListener { setEqualsClickListener() }
-            buttonDelete.setOnClickListener { setDeleteClickListener() }
+            buttonEquals.setOnClickListener { presenter.onEqualsClicked() }
+            buttonDelete.setOnClickListener { presenter.onDeleteClicked() }
         }
+    }
+
+    override fun showExpression(expression: String) {
+        binding.textView.text = expression
+    }
+
+    override fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun initOperandButtons() {
         with(binding) {
-            button0.setOnClickListener { expression.setOperand("0") }
-            button1.setOnClickListener { expression.setOperand("1") }
-            button2.setOnClickListener { expression.setOperand("2") }
-            button3.setOnClickListener { expression.setOperand("3") }
-            button4.setOnClickListener { expression.setOperand("4") }
-            button5.setOnClickListener { expression.setOperand("5") }
-            button6.setOnClickListener { expression.setOperand("6") }
-            button7.setOnClickListener { expression.setOperand("7") }
-            button8.setOnClickListener { expression.setOperand("8") }
-            button9.setOnClickListener { expression.setOperand("9") }
+            button0.setOnClickListener { presenter.onOperandClicked("0") }
+            button1.setOnClickListener { presenter.onOperandClicked("1") }
+            button2.setOnClickListener { presenter.onOperandClicked("2") }
+            button3.setOnClickListener { presenter.onOperandClicked("3") }
+            button4.setOnClickListener { presenter.onOperandClicked("4") }
+            button5.setOnClickListener { presenter.onOperandClicked("5") }
+            button6.setOnClickListener { presenter.onOperandClicked("6") }
+            button7.setOnClickListener { presenter.onOperandClicked("7") }
+            button8.setOnClickListener { presenter.onOperandClicked("8") }
+            button9.setOnClickListener { presenter.onOperandClicked("9") }
         }
     }
 
     private fun initOperatorButtons() {
         with(binding) {
-            buttonPlus.setOnClickListener { setOperatorClickListener(ArithmeticOperator.PLUS) }
-            buttonMinus.setOnClickListener { setOperatorClickListener(ArithmeticOperator.MINUS) }
-            buttonMultiply.setOnClickListener { setOperatorClickListener(ArithmeticOperator.MULTIPLY) }
-            buttonDivide.setOnClickListener { setOperatorClickListener(ArithmeticOperator.DIVIDE) }
+            buttonPlus.setOnClickListener { presenter.onOperatorClicked(ArithmeticOperator.PLUS) }
+            buttonMinus.setOnClickListener { presenter.onOperatorClicked(ArithmeticOperator.MINUS) }
+            buttonMultiply.setOnClickListener { presenter.onOperatorClicked(ArithmeticOperator.MULTIPLY) }
+            buttonDivide.setOnClickListener { presenter.onOperatorClicked(ArithmeticOperator.DIVIDE) }
         }
-    }
-
-    private fun setOperatorClickListener(operator: ArithmeticOperator) {
-        expression.setOperator(operator.value)
-    }
-
-    private fun setEqualsClickListener() {
-        kotlin.runCatching {
-            calculator.calculate(ArithmeticExpression(expression.value)).toString()
-        }.onSuccess { result ->
-            expression.setEquals(result)
-        }.onFailure { exception ->
-            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun setDeleteClickListener() {
-        expression.setDelete()
     }
 }
---
+++ app/src/main/java/camp/nextstep/edu/calculator/MainContract.kt
@@ -0,0 +1,20 @@
+package camp.nextstep.edu.calculator
+
+import camp.nextstep.edu.calculator.domain.ArithmeticOperator
+
+interface MainContract {
+
+    interface View {
+        var presenter: Presenter
+
+        fun showExpression(expression: String)
+        fun showToast(message: String)
+    }
+
+    interface Presenter {
+        fun onOperandClicked(operand: String)
+        fun onOperatorClicked(operator: ArithmeticOperator)
+        fun onEqualsClicked()
+        fun onDeleteClicked()
+    }
+}
