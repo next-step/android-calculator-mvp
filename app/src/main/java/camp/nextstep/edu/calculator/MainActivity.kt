c967bd5f1c98c4bd17d4f00116f0d8f878613be9
OYJ <89826723+oyj7677@users.noreply.github.com>
2023-08-05 21:21:25 +0900

    Step4 계산기 기능 UI테스트 (#210)
    * feat: 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자를 보여준다
    
    * step3 개선 사항
    - ParameterizedTest Class 분리
    - 코드 가독성 개선
    	- kotlin.runCatching{ } → runCatching{ }
    	- String.require() 사용 및 isEmpty확장함수 사용
    - enum 활용 개선
    	- Operator객체 반환하여 사용
    
    * step3 개선 사항
    - Operator 계산 전략 enum에 귀속
    - when절 삭제
    
    * step 4 계산기 기능 UI 테스트
    
    * step3, step4 코드 개선
     - UI테스트 입력값 주석으로 표시
     - 하나의 클래스에 when - then 반복 구성
     - Parameterized 테스트 클래스 분리
     - 코드 정리
     - 수식(formula)모듈 생성
     - enum에 람다식 사용
     - 에러표시 개선
    
    ---------
    
    Co-authored-by: OYJ <oyj7677@gmail.lcom>

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -1,27 +1,74 @@
 package camp.nextstep.edu.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
+import com.example.calculatorlib.Calculator
+import com.example.formulalib.Formula
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val calculate = Calculator()
+    private val formula = Formula()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener{binding.textView.text = "0"}
-        binding.button1.setOnClickListener{binding.textView.text = "1"}
-        binding.button2.setOnClickListener{binding.textView.text = "2"}
-        binding.button3.setOnClickListener{binding.textView.text = "3"}
-        binding.button4.setOnClickListener{binding.textView.text = "4"}
-        binding.button5.setOnClickListener{binding.textView.text = "5"}
-        binding.button6.setOnClickListener{binding.textView.text = "6"}
-        binding.button7.setOnClickListener{binding.textView.text = "7"}
-        binding.button8.setOnClickListener{binding.textView.text = "8"}
-        binding.button9.setOnClickListener{binding.textView.text = "9"}
+        binding.button0.setOnClickListener { addNumber('0') }
+        binding.button1.setOnClickListener { addNumber('1') }
+        binding.button2.setOnClickListener { addNumber('2') }
+        binding.button3.setOnClickListener { addNumber('3') }
+        binding.button4.setOnClickListener { addNumber('4') }
+        binding.button5.setOnClickListener { addNumber('5') }
+        binding.button6.setOnClickListener { addNumber('6') }
+        binding.button7.setOnClickListener { addNumber('7') }
+        binding.button8.setOnClickListener { addNumber('8') }
+        binding.button9.setOnClickListener { addNumber('9') }
 
+        binding.buttonPlus.setOnClickListener { addOperator('+') }
+        binding.buttonMinus.setOnClickListener { addOperator('-') }
+        binding.buttonMultiply.setOnClickListener { addOperator('*') }
+        binding.buttonDivide.setOnClickListener { addOperator('/') }
+        binding.buttonDelete.setOnClickListener { deleteLastStr() }
+        binding.buttonEquals.setOnClickListener { calculate() }
+    }
+
+    private fun calculate() {
+        if (isLastStrNum()) {
+            showText(calculate.evaluate(formula.getFormula()).toString())
+        } else {
+            Toast.makeText(this, "완성되지 않은 수식입니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // 텍스트 화면 출력
+    private fun showText(str: String) {
+        binding.textView.text = str
+    }
+
+    // 숫자 추가
+    private fun addNumber(str: Char) {
+        showText(formula.addNumber(str))
+    }
+
+    // 연산자 추가
+    // 직전 값이 연산자일 경우 교체한다.
+    private fun addOperator(str: Char) {
+        showText(formula.addOperator(str))
+    }
+
+    // 직전 값이 숫자인가
+    // return True: 숫자, False: 연산자
+    // 직전 값이 없다면?
+    private fun isLastStrNum(): Boolean {
+        return formula.isLastStrNum()
+    }
+
+    // 최근 항목 삭게
+    private fun deleteLastStr() {
+        showText(formula.deleteLastStr())
     }
 }
