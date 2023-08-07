816868482628a7c31b70d47c5bc7e4767f38d1ce
김영욱 <0.uk.kim@navercorp.com>
2023-08-07 17:42:51 +0900

    UI 테스트 공통 함수 분리

--- app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/camp/nextstep/edu/calculator/MainActivityTest.kt
@@ -16,106 +16,114 @@ class MainActivityTest {
 	@Test
 	fun 피연산자를_클릭하면__피연산자가_그대로노출된다() {
 		// when
-		onView(withId(R.id.button1)).perform(click())
+		clickButton(R.id.button1)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1")))
+		checkText(R.id.textView, "1")
 	}
 
 	@Test
 	fun 빈값일때__연산자를_클릭하면__빈값이_노출된다() {
 		// when
-		onView(withId(R.id.buttonPlus)).perform(click())
+		clickButton(R.id.buttonPlus)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("")))
+		checkText(R.id.textView, "")
 	}
 
 	@Test
 	fun 빈값이_아닐때__연산자를_클릭하면__좌우공백과함께_연산자가_노출된다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
+		clickButton(R.id.button1)
 
 		// when
-		onView(withId(R.id.buttonPlus)).perform(click())
+		clickButton(R.id.buttonPlus)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1 + ")))
+		checkText(R.id.textView, "1 + ")
 	}
 
 	@Test
 	fun 연산자가_입력되었을때__연산자를_클릭하면__연산자가_덮어씌여진다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
-		onView(withId(R.id.buttonPlus)).perform(click())
+		clickButton(R.id.button1)
+		clickButton(R.id.buttonPlus)
 
 		// when
-		onView(withId(R.id.buttonMultiply)).perform(click())
+		clickButton(R.id.buttonMultiply)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1 * ")))
+		checkText(R.id.textView, "1 * ")
 	}
 
 	@Test
 	fun 빈값일때__삭제버튼을_클릭하면__빈값이_노출된다() {
 		// when
-		onView(withId(R.id.buttonDelete)).perform(click())
+		clickButton(R.id.buttonDelete)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("")))
+		checkText(R.id.textView, "")
 	}
 
 	@Test
 	fun 피연산자가_입력되었을때__삭제버튼을_클릭하면__피연산자가_제거된다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
-		onView(withId(R.id.buttonPlus)).perform(click())
-		onView(withId(R.id.button2)).perform(click())
+		clickButton(R.id.button1)
+		clickButton(R.id.buttonPlus)
+		clickButton(R.id.button2)
 
 		// when
-		onView(withId(R.id.buttonDelete)).perform(click())
+		clickButton(R.id.buttonDelete)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1 + ")))
+		checkText(R.id.textView, "1 + ")
 	}
 
 	@Test
 	fun 연산자가_입력되었을때__삭제버튼을_클릭하면__좌우공백과함께_연산자가_제거된다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
-		onView(withId(R.id.buttonPlus)).perform(click())
+		clickButton(R.id.button1)
+		clickButton(R.id.buttonPlus)
 
 		// when
-		onView(withId(R.id.buttonDelete)).perform(click())
+		clickButton(R.id.buttonDelete)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1")))
+		checkText(R.id.textView, "1")
 	}
 
 	@Test
 	fun 피연산자가_입력되었을때__eqauls_버튼을_클릭하면__계산된결과가_노출된다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
-		onView(withId(R.id.buttonPlus)).perform(click())
-		onView(withId(R.id.button2)).perform(click())
+		clickButton(R.id.button1)
+		clickButton(R.id.buttonPlus)
+		clickButton(R.id.button2)
 
 		// when
-		onView(withId(R.id.buttonEquals)).perform(click())
+		clickButton(R.id.buttonEquals)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("3")))
+		checkText(R.id.textView, "3")
 	}
 
 	@Test
 	fun 연산자가_입력되었을때__equals_버튼을_클릭하면__계산식이_그대로_유지된다() {
 		// given
-		onView(withId(R.id.button1)).perform(click())
-		onView(withId(R.id.buttonPlus)).perform(click())
+		clickButton(R.id.button1)
+		clickButton(R.id.buttonPlus)
 
 		// when
-		onView(withId(R.id.buttonEquals)).perform(click())
+		clickButton(R.id.buttonEquals)
 
 		// then
-		onView(withId(R.id.textView)).check(matches(withText("1 + ")))
+		checkText(R.id.textView, "1 + ")
+	}
+
+	private fun clickButton(id: Int) {
+		onView(withId(id)).perform(click())
+	}
+
+	private fun checkText(id: Int, text: String) {
+		onView(withId(id)).check(matches(withText(text)))
 	}
 }
