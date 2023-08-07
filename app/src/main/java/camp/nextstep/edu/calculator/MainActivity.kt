20dafff648f971d585562130b42dc216890cd1d8
김영욱 <0.uk.kim@navercorp.com>
2023-08-07 18:10:34 +0900

    MVP 전환

--- app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import camp.nextstep.edu.calculator.databinding.ActivityMainBinding
-import camp.nextstep.edu.domain.CAST_INT_EXCEPTION
-import camp.nextstep.edu.domain.Calculator
 import camp.nextstep.edu.domain.Expression
 import camp.nextstep.edu.domain.Operator
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 	private lateinit var binding: ActivityMainBinding
 	override lateinit var presenter: MainContract.Presenter
-
-	private val calculator = Calculator()
-	private var expression = Expression()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -29,85 +24,47 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 		setEqualsListener()
 	}
 
-
 	private fun initMainPresenter() {
 		presenter = MainPresenterImpl(this)
 	}
 
 	private fun setOperandListener() {
-		binding.button0.setOnClickListener { insertOperand("0") }
-		binding.button1.setOnClickListener { insertOperand("1") }
-		binding.button2.setOnClickListener { insertOperand("2") }
-		binding.button3.setOnClickListener { insertOperand("3") }
-		binding.button4.setOnClickListener { insertOperand("4") }
-		binding.button5.setOnClickListener { insertOperand("5") }
-		binding.button6.setOnClickListener { insertOperand("6") }
-		binding.button7.setOnClickListener { insertOperand("7") }
-		binding.button8.setOnClickListener { insertOperand("8") }
-		binding.button9.setOnClickListener { insertOperand("9") }
-	}
-
-	private fun insertOperand(operand: String) {
-		expression += operand
-		showExpression()
+		binding.button0.setOnClickListener { presenter.plus("0") }
+		binding.button1.setOnClickListener { presenter.plus("1") }
+		binding.button2.setOnClickListener { presenter.plus("2") }
+		binding.button3.setOnClickListener { presenter.plus("3") }
+		binding.button4.setOnClickListener { presenter.plus("4") }
+		binding.button5.setOnClickListener { presenter.plus("5") }
+		binding.button6.setOnClickListener { presenter.plus("6") }
+		binding.button7.setOnClickListener { presenter.plus("7") }
+		binding.button8.setOnClickListener { presenter.plus("8") }
+		binding.button9.setOnClickListener { presenter.plus("9") }
 	}
 
 	private fun setOperatorListener() {
-		binding.buttonPlus.setOnClickListener { insertOperator(Operator.PLUS) }
-		binding.buttonMinus.setOnClickListener { insertOperator(Operator.MINUS) }
-		binding.buttonMultiply.setOnClickListener { insertOperator(Operator.MUL) }
-		binding.buttonDivide.setOnClickListener { insertOperator(Operator.DIV) }
-	}
-
-	private fun insertOperator(operator: Operator) {
-		expression += operator
-		showExpression()
+		binding.buttonPlus.setOnClickListener { presenter.plus(Operator.PLUS) }
+		binding.buttonMinus.setOnClickListener { presenter.plus(Operator.MINUS) }
+		binding.buttonMultiply.setOnClickListener { presenter.plus(Operator.MUL) }
+		binding.buttonDivide.setOnClickListener { presenter.plus(Operator.DIV) }
 	}
 
 	private fun setDeleteListener() {
-		binding.buttonDelete.setOnClickListener {
-			expression = expression.delete()
-			showExpression()
-		}
-	}
-
-	private fun showExpression() {
-		binding.textView.text = expression.toString()
+		binding.buttonDelete.setOnClickListener { presenter.delete() }
 	}
 
 	private fun setEqualsListener() {
-		binding.buttonEquals.setOnClickListener {
-			showResultOrThrow()
-		}
+		binding.buttonEquals.setOnClickListener { presenter.showResultOrThrow() }
 	}
 
-	private fun showResultOrThrow() {
-		runCatching {
-			calculator.calculate(expression.getCompleteExpression())
-		}.onSuccess {
-			showResult(it)
-		}.onFailure {
-			showToastIfNeed(it.message)
-		}
+	override fun showExpression(expression: Expression) {
+		binding.textView.text = expression.toString()
 	}
 
-	private fun showResult(result: Int) {
-		binding.textView.text = result.toString()
-		initExpression()
+	override fun showResult(result: String) {
+		binding.textView.text = result
 	}
 
-	private fun initExpression() {
-		expression = Expression()
-	}
-
-	private fun showToastIfNeed(errorMessage: String?) {
-		when (errorMessage) {
-			Expression.EXP_NOT_COMPLETE -> Toast.makeText(this, "완성되지 않은 수식입니다.", Toast.LENGTH_LONG).show()
-			Expression.EXP_IS_BLANK -> Toast.makeText(this, "계산식이 입력되지 않았습니다.", Toast.LENGTH_LONG).show()
-			Operator.INVALID_OPERATOR -> Toast.makeText(this, "유효하지 않은 연산자가 입력되었습니다.", Toast.LENGTH_LONG).show()
-			CAST_INT_EXCEPTION -> Toast.makeText(this, "숫자가 아닌 피연산자가 입력되었습니다.", Toast.LENGTH_LONG).show()
-			Calculator.DIVIDE_BY_ZERO -> Toast.makeText(this, "0으로 나눌 수 없습니다.", Toast.LENGTH_LONG).show()
-			else -> Unit
-		}
+	override fun showToast(message: String?) {
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 	}
 }
--- app/src/main/java/camp/nextstep/edu/calculator/MainPresenterImpl.kt
+++ app/src/main/java/camp/nextstep/edu/calculator/MainPresenterImpl.kt
@@ -1,7 +1,59 @@
 package camp.nextstep.edu.calculator
 
+import camp.nextstep.edu.domain.CAST_INT_EXCEPTION
+import camp.nextstep.edu.domain.Calculator
+import camp.nextstep.edu.domain.Expression
+import camp.nextstep.edu.domain.Operator
+
 class MainPresenterImpl(
 	private val view: MainContract.View
 ) : MainContract.Presenter {
 
+	private val calculator: Calculator = Calculator()
+	private var expression: Expression = Expression()
+
+	override fun plus(operand: String) {
+		expression += operand
+		view.showExpression(expression)
+	}
+
+	override fun plus(operator: Operator) {
+		expression += operator
+		view.showExpression(expression)
+	}
+
+	override fun delete() {
+		expression = expression.delete()
+		view.showExpression(expression)
+	}
+
+	override fun showResultOrThrow() {
+		runCatching {
+			calculator.calculate(expression.getCompleteExpression())
+		}.onSuccess {
+			showResult(it)
+		}.onFailure {
+			showToastIfNeed(it.message)
+		}
+	}
+
+	private fun showResult(result: Int) {
+		view.showResult(result.toString())
+		initExpression()
+	}
+
+	private fun initExpression() {
+		expression = Expression()
+	}
+
+	private fun showToastIfNeed(errorMessage: String?) {
+		when (errorMessage) {
+			Expression.EXP_NOT_COMPLETE -> view.showToast("완성되지 않은 수식입니다.")
+			Expression.EXP_IS_BLANK -> view.showToast("계산식이 입력되지 않았습니다.")
+			Operator.INVALID_OPERATOR -> view.showToast("유효하지 않은 연산자가 입력되었습니다.")
+			CAST_INT_EXCEPTION -> view.showToast("숫자가 아닌 피연산자가 입력되었습니다.")
+			Calculator.DIVIDE_BY_ZERO -> view.showToast("0으로 나눌 수 없습니다.")
+			else -> Unit
+		}
+	}
 }
